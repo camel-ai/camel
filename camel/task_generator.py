0e45456424ce0cc8b02983678655a2434065ca57
hammoudhasan <hasanabedalkader.hammoud@kaust.edu.sa>
2023-03-19 01:28:21 +0300

    Added data post processing

--- camel/task_generator.py
+++ camel/task_generator.py
@@ -7,33 +7,48 @@ from configs import (RoleNameGenerator, SystemMessageGenerator,
                      TaskPromptGenerator)
 
 
-def process_task(role_name: str, task_prompt: str) -> None:
+def process_task(role_name: str, task_generator_prompt: str,
+                 start_token: str = "1.", num_tasks: int = 40) -> None:
     sys_msg_generator = SystemMessageGenerator()
 
     assistant_sys_msg = sys_msg_generator.from_role(RoleType.DEFAULT)
     assistant_agent = ChatAgent(assistant_sys_msg, ModeType.GPT_3_5_TURBO)
 
-    assistant_sys_msg.content = task_prompt
+    assistant_sys_msg.content = task_generator_prompt
     user_msgs, _, _ = assistant_agent.step(assistant_sys_msg)
     user_msg = user_msgs[0]
 
+    tasks = user_msg.content.split("\n")
+
+    # Filter out the generated response to include the tasks only
+    for i, task in enumerate(tasks):
+        if start_token in task:
+            tasks = tasks[i:i + num_tasks]
+            break
+
+    # Ensure exact number of tasks is generated
+    assert str(num_tasks) in tasks[-1], print(tasks)
+
     with open(f"./tasks/{'_'.join(role_name)}.txt", "w") as file:
-        file.write(user_msg.content)
+        file.write("\n".join(tasks))
 
 
 def main() -> None:
     role_names_generator = RoleNameGenerator().from_role_files()
-    task_prompt_generator = TaskPromptGenerator().from_role_generator(
-        role_names_generator)
+    task_generator_prompt_generator = TaskPromptGenerator().from_role_files()
 
     pool = multiprocessing.Pool()
-    for task_prompt, role_name in zip(task_prompt_generator,
-                                      role_names_generator):
+    counter = 0
+    for task_generator_prompt, role_name in zip(
+            task_generator_prompt_generator, role_names_generator):
+        counter += 1
         if not os.path.exists(f"./tasks/{'_'.join(role_name)}.txt"):
-            pool.apply_async(process_task, (role_name, task_prompt))
+            print(f"Generating tasks for {role_name}")
+            pool.apply_async(process_task, (role_name, task_generator_prompt))
 
     pool.close()
     pool.join()
+    print(counter)
 
 
 if __name__ == "__main__":
