085f3013142d66d1c51cc8e45b0e368e95bc2257
lightaime <lightaime@gmail.com>
2023-03-20 22:42:39 +0300

    Add single agent for role prompts

---
+++ examples/single_agent.py
@@ -0,0 +1,21 @@
+from camel.agent import ChatAgent
+from camel.message import AssistantSystemMessage, UserChatMessage
+from camel.typing import ModeType
+
+
+def main(role_type: str = "user", num_roles: int = 50):
+    with open(f"prompts/generate_{role_type}.txt", "r") as f:
+        prompt = f.read().replace("<NUM_ROLES>", str(num_roles))
+    print(prompt)
+    assistant_sys_msg = AssistantSystemMessage(
+        "Assistant", content="You are a helpful assistant.")
+    agent = ChatAgent(assistant_sys_msg, model=ModeType.GPT_3_5_TURBO)
+    agent.reset()
+
+    user_msg = UserChatMessage("User", content=prompt)
+    assistant_msg, _, _ = agent.step(user_msg)
+    print(assistant_msg[0].content)
+
+
+if __name__ == "__main__":
+    main()
