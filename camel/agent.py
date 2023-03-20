dcc8734a62995b39ce3fefbb4c94a59511450d85
Guohao Li <lightaime@gmail.com>
2023-03-21 02:51:11 +0300

    Add `RolePlaying` class (#15)

--- camel/agent.py
+++ camel/agent.py
@@ -4,8 +4,9 @@ import openai
 from tenacity import retry, stop_after_attempt, wait_fixed
 
 from camel.configs import ChatGPTConfig
-from camel.message import (ChatMessage, MessageType, SystemMessage,
-                           UserChatMessage)
+from camel.generator import SystemMessageGenerator
+from camel.message import (AssistantChatMessage, ChatMessage, MessageType,
+                           SystemMessage, UserChatMessage)
 from camel.typing import ModeType, RoleType
 from camel.utils import get_model_token_limit, num_tokens_from_messages
 
@@ -145,3 +146,78 @@ class TaskSpecifyAgent(ChatAgent):
             raise RuntimeError("Task specification failed.")
         else:
             return specified_task_msg.content
+
+
+class RolePlaying:
+
+    def __init__(
+        self,
+        assistant_role_name: str,
+        user_role_name: str,
+        task_prompt: str = "",
+        with_task: bool = True,
+        with_task_specify: bool = True,
+        mode_type: ModeType = ModeType.GPT_3_5_TURBO,
+        assistant_agent_kwargs: Optional[Dict] = None,
+        user_agent_kwargs: Optional[Dict] = None,
+    ) -> None:
+        if with_task_specify:
+            task_specify_agent = TaskSpecifyAgent(
+                ModeType.GPT_3_5_TURBO, ChatGPTConfig(temperature=1.4))
+            self.task_prompt = task_specify_agent.specify_task(task_prompt)
+
+        sys_msg_generator = SystemMessageGenerator(with_task=with_task)
+        self.assistant_sys_msg, self.user_sys_msg = (
+            sys_msg_generator.from_roles(
+                roles=[
+                    (assistant_role_name, RoleType.ASSISTANT),
+                    (user_role_name, RoleType.USER),
+                ], task_prompt=task_prompt))
+
+        self.assistant_agent = ChatAgent(
+            self.assistant_sys_msg,
+            mode_type,
+            **(assistant_agent_kwargs or {}),
+        )
+        self.user_agent = ChatAgent(
+            self.user_sys_msg,
+            mode_type,
+            **(user_agent_kwargs or {}),
+        )
+
+    def init_chat(self) -> Tuple[AssistantChatMessage, List[ChatMessage]]:
+        self.assistant_agent.reset()
+        self.user_agent.reset()
+
+        # Send the system messages again to the agents using chat messages
+        assistant_msg = AssistantChatMessage(
+            self.assistant_agent.role_name,
+            content=(f"{self.user_sys_msg.content}. "
+                     "Now start to give me introductions one by one. "
+                     "Only reply with Instruction and Input."))
+        assistant_msg.role = "user"
+
+        user_msg = UserChatMessage(self.user_sys_msg.role_name,
+                                   content=f"{self.assistant_sys_msg.content}")
+        msgs, _, _ = self.assistant_agent.step(user_msg)
+
+        return assistant_msg, msgs
+
+    def step(
+        self,
+        assistant_msg: ChatMessage,
+    ) -> Tuple[ChatMessage, ChatMessage]:
+        user_msgs, user_terminated, _ = self.user_agent.step(assistant_msg)
+        if user_terminated:
+            raise RuntimeError("User agent is terminated.")
+        user_msg = user_msgs[0]
+        user_msg.role = "user"
+
+        assistant_msgs, assistant_terminated, _ = self.assistant_agent.step(
+            user_msg)
+        if assistant_terminated:
+            raise RuntimeError("Assistant agent is terminated.")
+        assistant_msg = assistant_msgs[0]
+        assistant_msg.role = "user"
+
+        return assistant_msg, user_msg
--- examples/role_playing.py
+++ examples/role_playing.py
@@ -1,71 +1,19 @@
-from typing import List, Tuple
-
-from camel.agent import ChatAgent, TaskSpecifyAgent
-from camel.configs import ChatGPTConfig
-from camel.generator import SystemMessageGenerator
-from camel.message import (AssistantChatMessage, AssistantSystemMessage,
-                           ChatMessage, UserChatMessage, UserSystemMessage)
-from camel.typing import ModeType, RoleType
-
-
-def init_chat(
-    assistant_agent: ChatAgent,
-    user_agent: ChatAgent,
-    user_sys_msg: UserSystemMessage,
-    assistant_sys_msg: AssistantSystemMessage,
-) -> Tuple[AssistantChatMessage, List[ChatMessage]]:
-    assistant_agent.reset()
-    user_agent.reset()
-
-    # Send the system messages again to the agents using chat messages
-    assistant_msg = AssistantChatMessage(
-        "Computer Programer",
-        content=(f"{user_sys_msg.content}. "
-                 "Now start to give me introductions one by one. "
-                 "Only reply with Instruction and Input."))
-    assistant_msg.role = "user"
-
-    user_msg = UserChatMessage(user_sys_msg.role_name,
-                               content=f"{assistant_sys_msg.content}")
-    msgs, _, _ = assistant_agent.step(user_msg)
-
-    return assistant_msg, msgs
+from camel.agent import RolePlaying
 
 
 def main() -> None:
-    original_task_prompt = "Developing custom game mods or plugins"
-    print(f"Original task prompt:\n{original_task_prompt}\n")
-    task_specify_agent = TaskSpecifyAgent(ModeType.GPT_3_5_TURBO,
-                                          ChatGPTConfig(temperature=1.4))
-    specified_task_prompt = task_specify_agent.specify_task(
-        original_task_prompt)
-    print(f"Specified task prompt:\n{specified_task_prompt}\n")
+    task_prompt = "Developing custom game mods or plugins"
+    print(f"Original task prompt:\n{task_prompt}\n")
+    role_play_session = RolePlaying("Computer Programer", "Gamer", task_prompt)
+    print(f"Specified task prompt:\n{role_play_session.task_prompt}\n")
 
-    sys_msg_generator = SystemMessageGenerator(with_task=True)
-    assistant_sys_msg, user_sys_msg = sys_msg_generator.from_roles(
-        roles=[("Computer Programmer", RoleType.ASSISTANT),
-               ("Gamer", RoleType.USER)], task_prompt=specified_task_prompt)
-    assistant_agent = ChatAgent(assistant_sys_msg, ModeType.GPT_3_5_TURBO)
-    user_agent = ChatAgent(user_sys_msg, ModeType.GPT_3_5_TURBO)
-
-    assistant_msg, _ = init_chat(assistant_agent, user_agent, user_sys_msg,
-                                 assistant_sys_msg)
-
-    while True:
-        user_msgs, user_terminated, _ = user_agent.step(assistant_msg)
-        if user_terminated:
-            break
-        user_msg = user_msgs[0]
-        print(f"User:\n{user_msg.content}\n")
-        user_msg.role = "user"
-
-        assistant_msgs, assistant_terminated, _ = assistant_agent.step(
-            user_msg)
-        if assistant_terminated:
-            break
-        assistant_msg = assistant_msgs[0]
-        print(f"Assistant:\n{assistant_msg.content}\n")
-        assistant_msg.role = "user"
+    chat_turn_limit, n = 10, 0
+    assistant_msg, _ = role_play_session.init_chat()
+    while n < chat_turn_limit:
+        n += 1
+        assistant_msg, user_msg = role_play_session.step(assistant_msg)
+        print(f"AI User:\n\n{user_msg.content}\n\n")
+        print(f"AI Assistant:\n\n{assistant_msg.content}\n\n")
 
 
 if __name__ == "__main__":
