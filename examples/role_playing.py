6a18c335e2b0916f6ef2d97263d46b4aa2f6bed2
lightaime <lightaime@gmail.com>
2023-03-21 17:29:57 +0300

    Added color print and optional window size

--- examples/role_playing.py
+++ examples/role_playing.py
@@ -1,5 +1,7 @@
 import time
 
+from colorama import Fore
+
 from camel.agent import RolePlaying
 
 
@@ -11,18 +13,20 @@ def print_text_animated(text):
 
 def main() -> None:
     task_prompt = "Design a custom game using PyGame"
-    print(f"Original task prompt:\n{task_prompt}\n")
+    print(Fore.YELLOW + f"Original task prompt:\n{task_prompt}\n")
     role_play_session = RolePlaying("Computer Programer", "Gamer", task_prompt)
-    print(f"Specified task prompt:\n{role_play_session.task_prompt}\n")
+    print(Fore.CYAN +
+          f"Specified task prompt:\n{role_play_session.task_prompt}\n")
 
     chat_turn_limit, n = 10, 0
     assistant_msg, _ = role_play_session.init_chat()
     while n < chat_turn_limit:
         n += 1
         assistant_msg, user_msg = role_play_session.step(assistant_msg)
-        print_text_animated(f"AI User:\n\n{user_msg.content}\n\n")
+        print_text_animated(Fore.BLUE + f"AI User:\n\n{user_msg.content}\n\n")
         time.sleep(0.5)
-        print_text_animated(f"AI Assistant:\n\n{assistant_msg.content}\n\n")
+        print_text_animated(Fore.GREEN +
+                            f"AI Assistant:\n\n{assistant_msg.content}\n\n")
 
         if "<CAMEL_TASK_DONE>" in assistant_msg.content:
             break
