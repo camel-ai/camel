879e17a8f91a1b07da9761fe65dd0debe2d1235d
Dmitrii Khizbullin <dmitrii.khizbullin@gmail.com>
2023-05-10 00:57:19 +0300

    Extracted database connection class

--- apps/dilemma/dilemma.py
+++ apps/dilemma/dilemma.py
@@ -8,6 +8,7 @@ from functools import partial
 from typing import Any, Dict, List, Optional, Tuple
 
 import gradio as gr
+from database_connection import DatabaseConnection
 
 
 def parse_arguments():
@@ -36,124 +37,6 @@ def parse_arguments():
     return args
 
 
-import os
-
-import sqlalchemy
-from google.cloud.sql.connector import Connector, IPTypes
-
-# import google.auth
-# from google.auth.transport.requests import Request
-
-
-def connect_db():
-    INSTANCE_CONNECTION_NAME = "camel-lm:me-central1:camel-dilemma"
-    print(f"Your instance connection name is: {INSTANCE_CONNECTION_NAME}")
-    DB_USER = "dkuser"
-    DB_PASS = "camel230509"
-    DB_NAME = "dilemma_choices"
-
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = \
-        "/home/ivul_kaust/camel-lm-57fc849b664b.json"
-
-    # ip_addr = "34.18.39.52"
-
-    # initialize Connector object
-    connector = Connector()
-
-    # # function to return the database connection object
-    # def getconn():
-    #     conn = connector.connect(INSTANCE_CONNECTION_NAME, "pymysql",
-    #                              user=DB_USER, password=DB_PASS, db=DB_NAME,
-    #                              key="AIzaSyDys_eiXMOxf6aPz3AGqidlNk-oNqK7LTM")
-    #     return conn
-
-    # IAM database user parameter (IAM user's email before the "@" sign, mysql truncates usernames)
-    # ex. IAM user with email "demo-user@test.com" would have database username "demo-user"
-    # IAM_USER = current_user[0].split("@")[0]
-    IAM_USER = "ivul.kaust"
-
-    # getconn now using IAM user and requiring no password with IAM Auth enabled
-    #def getconn():
-    #    conn = connector.connect(
-    #        INSTANCE_CONNECTION_NAME,
-    #        "pymysql",
-    #        user=IAM_USER,
-    #        db="",  # log in to instance but don't connect to specific database
-    #        enable_iam_auth=True)
-    #    return conn
-
-    # getconn now set to private IP
-    def getconn():
-        conn = connector.connect(
-            INSTANCE_CONNECTION_NAME, # <PROJECT-ID>:<REGION>:<INSTANCE-NAME>
-            "pymysql",
-            user=DB_USER,
-            password=DB_PASS,
-            db=DB_NAME,
-            ip_type=IPTypes.PRIVATE
-        )
-        return conn
-
-    # create connection pool with 'creator' argument to our connection object function
-    pool = sqlalchemy.create_engine(
-        "mysql+pymysql://",
-        creator=getconn,
-    )
-
-    # connect to connection pool
-    with pool.connect() as db_conn:
-        # create ratings table in our sandwiches database
-        db_conn.execute(
-            sqlalchemy.text(
-                "CREATE TABLE IF NOT EXISTS ratings "
-                "( id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, "
-                "origin VARCHAR(255) NOT NULL, rating FLOAT NOT NULL, "
-                "PRIMARY KEY (id));"))
-
-        # commit transaction (SQLAlchemy v2.X.X is commit as you go)
-        db_conn.commit()
-
-        # insert data into our ratings table
-        insert_stmt = sqlalchemy.text(
-            "INSERT INTO ratings (name, origin, rating) VALUES (:name, :origin, :rating)",
-        )
-
-        # insert entries into table
-        db_conn.execute(
-            insert_stmt, parameters={
-                "name": "HOTDOG",
-                "origin": "Germany",
-                "rating": 7.5
-            })
-        db_conn.execute(
-            insert_stmt, parameters={
-                "name": "BÀNH MÌ",
-                "origin": "Vietnam",
-                "rating": 9.1
-            })
-        db_conn.execute(
-            insert_stmt, parameters={
-                "name": "CROQUE MADAME",
-                "origin": "France",
-                "rating": 8.3
-            })
-
-        # commit transactions
-        db_conn.commit()
-
-        # query and fetch ratings table
-        results = db_conn.execute(
-            sqlalchemy.text("SELECT * FROM ratings")).fetchall()
-
-        # show results
-        for row in results:
-            print(row)
-
-    connector.close()
-
-    print("")
-
-
 def construct_ui(blocks, dataset: Any):
     """ Build Gradio UI and populate with texts from TXTs.
 
@@ -165,10 +48,7 @@ def construct_ui(blocks, dataset: Any):
         None
     """
 
-    connect_db()
-
-    def write_db(choice: str, left: str, right: str):
-        print(choice, left, right)
+    db_conn = DatabaseConnection()
 
     gr.Markdown("## Dilemma app")
     with gr.Row():
@@ -185,9 +65,10 @@ def construct_ui(blocks, dataset: Any):
         return (f"txt {random.randint(1, 100)}",
                 f"txt {random.randint(1, 100)}")
 
-    def record(choise: str, left: str, right: str):
-        assert choise in {'left', 'draw', 'right'}
-        write_db(choise, left, right)
+    def record(choice: str, left: str, right: str):
+        assert choice in {'left', 'draw', 'right'}
+        print(choice, left, right)
+        db_conn.add_record(choice, left, right)
 
     left_better_bn.click(partial(record, 'left'), [left_md, right_md], None) \
         .then(load_random, None, [left_md, right_md])
