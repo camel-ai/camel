e4fc392e5eabd7c4ff2e2b7e747094f40f72d616
lightaime <lightaime@gmail.com>
2023-05-11 23:31:55 +0300

    update

--- test/agents/test_role_playing.py
+++ test/agents/test_role_playing.py
@@ -39,8 +39,8 @@ def test_role_playing_init():
 @pytest.mark.parametrize(
     "task_type, extend_sys_msg_meta_dicts, extend_task_specify_meta_dict",
     [(TaskType.AI_SOCIETY, None, None),
-     (TaskType.CODE, [dict(domain="math", language="python")] * 2,
-      dict(domain="math", language="python")),
+     (TaskType.CODE, [dict(domain="science", language="python")] * 2,
+      dict(domain="science", language="python")),
      (TaskType.MISALIGNMENT, None, None)])
 def test_role_playing_step(task_type, extend_sys_msg_meta_dicts,
                            extend_task_specify_meta_dict):
