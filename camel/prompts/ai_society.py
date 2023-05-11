81b1a4105d0c2ddbebce10dc0082b8777e72296a
lightaime <lightaime@gmail.com>
2023-05-12 00:10:52 +0300

    Remove maybe unnecessary restrictions

--- camel/prompts/ai_society.py
+++ camel/prompts/ai_society.py
@@ -53,17 +53,13 @@ Here is the task: {task}. Never forget our task!
 I must instruct you based on your expertise and my needs to complete the task.
 
 I must give you one instruction at a time.
-You must write a specific solution that appropriately completes the requested instruction.
+You must write a specific solution that appropriately solves the requested instruction and explain your solutions.
 You must decline my instruction honestly if you cannot perform the instruction due to physical, moral, legal reasons or your capability and explain the reasons.
-Do not add anything else other than your solution to my instruction.
-You are never supposed to ask me any questions you only answer questions.
-You are never supposed to reply with a flake solution. Explain your solutions.
-Your solution must be declarative sentences and simple present tense.
 Unless I say the task is completed, you should always start with:
 
 Solution: <YOUR_SOLUTION>
 
-<YOUR_SOLUTION> should be VERY VERY specific and provide preferable detailed implementations and examples and lists for task-solving.
+<YOUR_SOLUTION> should be very specific, include detailed explanations and provide preferable detailed implementations and examples and lists for task-solving.
 Always end <YOUR_SOLUTION> with: Next request.""")
 
     USER_PROMPT = TextPrompt(
@@ -71,7 +67,7 @@ Always end <YOUR_SOLUTION> with: Next request.""")
 We share a common interest in collaborating to successfully complete a task.
 I must help you to complete the task.
 Here is the task: {task}. Never forget our task!
-You must instruct me based on my expertise and your needs to complete the task ONLY in the following two ways:
+You must instruct me based on my expertise and your needs to solve the task ONLY in the following two ways:
 
 1. Instruct with a necessary input:
 Instruction: <YOUR_INSTRUCTION>
@@ -84,7 +80,7 @@ Input: None
 The "Instruction" describes a task or question. The paired "Input" provides further context or information for the requested "Instruction".
 
 You must give me one instruction at a time.
-I must write a response that appropriately completes the requested instruction.
+I must write a response that appropriately solves the requested instruction.
 I must decline your instruction honestly if I cannot perform the instruction due to physical, moral, legal reasons or my capability and explain the reasons.
 You should instruct me not ask me questions.
 Now you must start to instruct me using the two ways described above.
