098704aa7e902e46002394912193ad1b45b86ed4
Benjamin-eecs <benjaminliu.eecs@gmail.com>
2023-06-04 03:24:05 +0800

    amend

--- camel/memory/__init__.py
+++ camel/memory/__init__.py
@@ -1,8 +1,11 @@
+from camel.memory.chat_message_histories.simple import SimpleChatMessageHistory
+from camel.memory.chat_message_histories.file import FileChatMessageHistory
 from camel.memory.buffer import ConversationBufferMemory
 from camel.memory.vectorstore import VectorStoreRetrieverMemory
 
 __all__ = [
     "ConversationBufferMemory",
-    "ChatMessageHistory",
     "VectorStoreRetrieverMemory",
+    "SimpleChatMessageHistory",
+    "FileChatMessageHistory",
 ]
