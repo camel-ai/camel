81377379118cf73bb91eb0e9252082da14f499dd
Dmitrii Khizbullin <dmitrii.khizbullin@gmail.com>
2023-07-05 12:54:13 +0300

    Fix mypy warnings

--- camel/agents/embodied_agent.py
+++ camel/agents/embodied_agent.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Sequence
 
 from colorama import Fore
 from overrides import override
@@ -59,7 +59,8 @@ class EmbodiedAgent(ChatAgent):
         default_action_space = [
             HuggingFaceToolAgent('hugging_face_tool_agent', model=model.value),
         ]
-        self.action_space = action_space or default_action_space
+        self.action_space: Sequence[
+            BaseToolAgent] = action_space or default_action_space
         action_space_prompt = self.get_action_space_prompt()
         system_message.content = system_message.content.format(
             action_space=action_space_prompt)
