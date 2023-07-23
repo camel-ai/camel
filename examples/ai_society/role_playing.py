ba23cedfe0306f4b96a57c7967759e036181cfe1
Zikang CHEN <appointat@shu.edu.cn>
2023-07-23 13:59:05 +0200

    Revert role_playing

--- examples/ai_society/role_playing.py
+++ examples/ai_society/role_playing.py
@@ -1,25 +1,30 @@
+# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+# Licensed under the Apache License, Version 2.0 (the “License”);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
 from colorama import Fore
 
 from camel.agents import RolePlaying
-from camel.agents.task_agent import RoleAssignmentAgent
-from camel.typing import ModelType
 from camel.utils import print_text_animated
 
 
-def main() -> None:
-    task_prompt = "Developing a trading bot for stock market"
-
-    role_assignment_agent = RoleAssignmentAgent(model=ModelType.GPT_3_5_TURBO)
-
-    roles = role_assignment_agent.step(task_prompt)
-    ai_user_role = roles[0]
-    ai_assistant_role = roles[1]
-
+def main(model_type=None) -> None:
+    task_prompt = "Develop a trading bot for the stock market"
     role_play_session = RolePlaying(
-        ai_assistant_role,
-        ai_user_role,
+        "Python Programmer",
+        "Stock Trader",
         task_prompt=task_prompt,
         with_task_specify=True,
+        model_type=model_type,
     )
 
     print(
@@ -35,29 +40,31 @@ def main() -> None:
     print(Fore.RED + f"Final task prompt:\n{role_play_session.task_prompt}\n")
 
     chat_turn_limit, n = 50, 0
-    assistant_msg, _ = role_play_session.init_chat()
+    input_assistant_msg, _ = role_play_session.init_chat()
     while n < chat_turn_limit:
         n += 1
-        assistant_return, user_return = role_play_session.step(assistant_msg)
-        assistant_msg, assistant_terminated, assistant_info = assistant_return
-        user_msg, user_terminated, user_info = user_return
+        assistant_response, user_response = role_play_session.step(
+            input_assistant_msg)
 
-        if assistant_terminated:
+        input_assistant_msg = assistant_response.msg
+
+        if assistant_response.terminated:
             print(Fore.GREEN +
-                  ("AI Assistant terminated. "
-                   f"Reason: {assistant_info['termination_reasons']}."))
+                  ("AI Assistant terminated. Reason: "
+                   f"{assistant_response.info['termination_reasons']}."))
             break
-        if user_terminated:
+        if user_response.terminated:
             print(Fore.GREEN +
                   ("AI User terminated. "
-                   f"Reason: {user_info['termination_reasons']}."))
+                   f"Reason: {user_response.info['termination_reasons']}."))
             break
 
-        print_text_animated(Fore.BLUE + f"AI User:\n\n{user_msg.content}\n")
-        print_text_animated(Fore.GREEN +
-                            f"AI Assistant:\n\n{assistant_msg.content}\n")
+        print_text_animated(Fore.BLUE +
+                            f"AI User:\n\n{user_response.msg.content}\n")
+        print_text_animated(Fore.GREEN + "AI Assistant:\n\n"
+                            f"{assistant_response.msg.content}\n")
 
-        if "CAMEL_TASK_DONE" in user_msg.content:
+        if "CAMEL_TASK_DONE" in user_response.msg.content:
             break
 
 
