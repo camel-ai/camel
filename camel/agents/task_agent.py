ac00d54312063d25228e7e75c35f9540d9a2210a
Hongyi Chen <59629242+hychen-naza@users.noreply.github.com>
2023-07-24 16:09:06 -0400

    Update camel/agents/task_agent.py
    Co-authored-by: Zecheng Zhang <zecheng@alumni.stanford.edu>

--- camel/agents/task_agent.py
+++ camel/agents/task_agent.py
@@ -311,7 +311,7 @@ class TaskPrioritizeAgent(ChatAgent):
         model_config (Any): The configuration for the model.
             (default: :obj:`None`)
         output_language (str, optional): The language to be output by the
-        agent. (default: :obj:`None`)
+            agent. (default: :obj:`None`)
         objective: The objective for the AI to perform the task.
     """
 
