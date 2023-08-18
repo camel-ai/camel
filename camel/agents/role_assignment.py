b702c771f0f1540f4a445e3b5ba02edd56ea63d5
Appointat <65004114+Appointat@users.noreply.github.com>
2023-08-18 11:25:26 +0200

    Update camel/agents/role_assignment.py
    Co-authored-by: Guohao Li <lightaime@gmail.com>

--- camel/agents/role_assignment.py
+++ camel/agents/role_assignment.py
@@ -31,8 +31,8 @@ class RoleAssignmentAgent(ChatAgent):
     Args:
         model (ModelType, optional): The type of model to use for the agent.
             (default: :obj:`ModelType.GPT_3_5_TURBO`)
-        model_config (Any): The configuration for the model.
-            (default: :obj: 'None')
+        model_config (Any, optional): The configuration for the model.
+            (default: :obj:`None`)
     """
 
     def __init__(
