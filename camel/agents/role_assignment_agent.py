44cdbdb6437c2f708b632cd503155abae72a27e1
Zikang CHEN <appointat@shu.edu.cn>
2023-09-08 00:43:22 +0200

    Add the feature of task split

--- camel/agents/role_assignment_agent.py
+++ camel/agents/role_assignment_agent.py
@@ -12,7 +12,7 @@
 # limitations under the License.
 # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
 import re
-from typing import Any, Dict, Optional, Union
+from typing import Any, Dict, List, Optional, Union
 
 from tenacity import retry, stop_after_attempt, wait_exponential
 
@@ -25,6 +25,9 @@ from camel.typing import ModelType, RoleType
 class RoleAssignmentAgent(ChatAgent):
     r"""An agent that generates role names based on the task prompt.
     Attributes:
+        role_descriptions_dict (Dict[str, str]): A dictionary mapping role
+            names to their descriptions.
+        subtasks (List[str]): The subtasks to complete the whole task.
         role_assignment_prompt (TextPrompt): A prompt for the agent to generate
         role names.
 
@@ -40,6 +43,9 @@ class RoleAssignmentAgent(ChatAgent):
         model: ModelType = ModelType.GPT_3_5_TURBO,
         model_config: Optional[Any] = None,
     ) -> None:
+        self.role_descriptions_dict = {}
+        self.subtasks = []
+
         system_message = BaseMessage(
             role_name="Role Assigner",
             role_type=RoleType.ASSISTANT,
@@ -118,3 +124,82 @@ class RoleAssignmentAgent(ChatAgent):
         }
 
         return role_descriptions_dict
+
+    def run_task_assignment():
+        # TODO: Implement task assignment
+        print("Not implemented yet.")
+
+    @retry(wait=wait_exponential(min=5, max=60), stop=stop_after_attempt(5))
+    def split_tasks(
+        self,
+        task_prompt: Union[str, TextPrompt],
+        num_subtasks: Optional[int] = None,
+        role_descriptions_dict: Optional[Dict[str, str]] = None,
+    ) -> List[str]:
+        r"""Split the task into subtasks based on the input task prompt.
+
+        Args:
+            task_prompt (Union[str, TextPrompt]): The prompt
+                for the task based on which the roles are to be generated.
+            num_subtasks (Optional[int], optional): The number of subtasks to
+                generate. (default: :obj:`None`)
+            role_descriptions_dict (Optional[Dict[str, str]], optional): The
+                role descriptions of each role. (default: :obj:`None`)
+
+        Returns:
+            List[str]: The subtasks to complete the whole task.
+        """
+        role_descriptions_dict = (role_descriptions_dict
+                                  or self.role_descriptions_dict)
+        role_names = list(role_descriptions_dict.keys())
+
+        task_prompt = TextPrompt("===== TASK =====\n" + task_prompt + "\n\n")
+        role_with_description_prompt = \
+            "===== ROLES WITH DESCRIPTION =====\n" + "\n".join(
+                f"{role_name}:\n{role_descriptions_dict[role_name]}\n"
+                for role_name in role_names) + "\n\n"
+        answer_prompt = "===== ANSWER PROMPT =====\n" + "\n".join(
+            f"Content of subtask {i + 1}: <BLANK>"
+            for i in range(num_subtasks or 0))
+        splict_task_prompt = TextPrompt(
+            "You are a role assignment agent, and you're in asked with " +
+            "dividing the main TASK into {num_subtasks} subtasks. " +
+            "In your team consists of {num_roles} domain experts each " +
+            "contributing to the {num_subtasks} subtasks.\n\n" + task_prompt +
+            role_with_description_prompt +
+            "Your answer MUST adhere to the format of ANSWER PROMPT, and " +
+            "ONLY answer the content of subtask without roles in BLANKs.\n" +
+            answer_prompt)
+        print(
+            splict_task_prompt.format(num_subtasks=num_subtasks,
+                                      num_roles=len(role_names)))
+
+        if num_subtasks is None:
+            subtasks_generation = splict_task_prompt.format(
+                num_subtasks=num_subtasks, num_roles=len(role_names))
+        else:
+            subtasks_generation = splict_task_prompt.format(
+                num_subtasks=num_subtasks, num_roles=len(role_names))
+        subtasks_generation_msg = BaseMessage.make_user_message(
+            role_name="Task Splitter", content=subtasks_generation)
+
+        response = super().step(input_message=subtasks_generation_msg)
+
+        msg = response.msg  # type: BaseMessage
+        terminated = response.terminated
+
+        # Distribute the output completions into subtasks
+        subtasks = [
+            desc.replace("<|", "").replace("|>", "")
+            for desc in re.findall(r"Content of subtask \d: (.+?)(?=\n|$)",
+                                   msg.content, re.DOTALL)
+        ]
+
+        if num_subtasks is not None and len(subtasks) != num_subtasks:
+            raise RuntimeError(
+                "Got None or insufficient information of subtasks.")
+        if terminated:
+            raise RuntimeError("Subtask split failed.")
+
+        self.subtasks = subtasks
+        return subtasks
