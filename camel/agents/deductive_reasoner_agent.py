24bfe0780262053e85ee0d2f4708c638c2acc007
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2023-10-22 17:46:13 +0200

    Add labels in deduction

--- camel/agents/deductive_reasoner_agent.py
+++ camel/agents/deductive_reasoner_agent.py
@@ -95,31 +95,36 @@ You are tasked with understanding a mathematical model based on the components $
 
 ===== THOUGHT OF DEDUCTIVE REASONING =====
 1. Define the Parameters of A and B:
-- Characterization: Before delving into transitions, thoroughly understand the nature and boundaries of both $A$ and $B$. This includes the type, properties, constraints, and possible interactions between the two.
-- Contrast and Compare: Highlight the similarities and differences between $A$ and $B$. This comparative analysis will give an insight into what needs changing and what remains constant.
+    - Characterization: Before delving into transitions, thoroughly understand the nature and boundaries of both $A$ and $B$. This includes the type, properties, constraints, and possible interactions between the two.
+    - Contrast and Compare: Highlight the similarities and differences between $A$ and $B$. This comparative analysis will give an insight into what needs changing and what remains constant.
 2. Historical & Empirical Analysis:
-- Previous Transitions according to the Knowledge Base of GPT: (if applicable) Extract conditions and patterns from the historical instances where a similar transition from a state comparable to $A$ moved towards $B$.
-- Scientific Principles: (if applicable) Consider the underlying scientific principles governing or related to the states and their transition. For example, if $A$ and $B$ are physical states, laws of physics might apply.
+    - Previous Transitions according to the Knowledge Base of GPT: (if applicable) Extract conditions and patterns from the historical instances where a similar transition from a state comparable to $A$ moved towards $B$.
+    - Scientific Principles: (if applicable) Consider the underlying scientific principles governing or related to the states and their transition. For example, if $A$ and $B$ are physical states, laws of physics might apply.
 3. Logical Deduction of Conditions ($C$):
-- Direct Path Analysis: What are the immediate and direct conditions required to move from $A$ to $B$?
-- Intermediate States: Are there states between $A$ and $B$ that must be transversed or can be used to make the transition smoother or more efficient? If yes, what is the content?
-- Constraints & Limitations: Identify potential barriers or restrictions in moving from $A$ to $B$. These can be external (e.g., environmental factors) or internal (properties of $A$ or $B$).
-- Resource and Information Analysis: What resources and information are required for the transition? This could be time, entity, factor, code language, software platform, unknowns, etc.
-- External Influences: Consider socio-economic, political, or environmental factors (if applicable) that could influence the transition conditions.
-- Creative/Heuristic Reasoning: Open your mind to multiple possible $C$'s, no matter how unconventional they might seem. Utilize analogies, metaphors, or brainstorming techniques to envision possible conditions or paths from $A$ to $B$.
-- The conditions $C$ should be multiple but in one sentence. And each condition should be concerned with one aspect/entity.
-4. Quality Assessment ($Q$):
-- Efficiency: How efficient is the transition from $A$ to $B$, which measures the resources used versus the desired outcome?
-- Effectiveness: Did the transition achieve the desired outcome or was the target state achieved as intended?
-- Safety & Risks: Assess any risks associated with the transition and the measures to mitigate them.
-- Feedback Mechanisms: Incorporate feedback loops to continuously monitor and adjust the quality of transition, making it more adaptive.
-5. Iterative Evaluation:
-- Test & Refine: Based on the initially deduced conditions and assessed quality, iterate the process to refine and optimize the transition. This might involve tweaking conditions, employing different paths, or changing resources.
-- Feedback Integration: Use feedback to make improvements and increase the quality of the transition.
-6. Real-world scenarios often present challenges that may not be captured by models and frameworks. While using the model, maintain an adaptive mindset:
-- Scenario Exploration: Continuously imagine various possible scenarios, both positive and negative, to prepare for unexpected events.
-- Flexibility: Be prepared to modify conditions ($C$) or alter the path/process ($L$) if unforeseen challenges arise.
-- Feedback Integration: Rapidly integrate feedback from actual implementations to adjust the model's application, ensuring relevancy and effectiveness.
+    - Direct Path Analysis: What are the immediate and direct conditions required to move from $A$ to $B$?
+    - Intermediate States: Are there states between $A$ and $B$ that must be transversed or can be used to make the transition smoother or more efficient? If yes, what is the content?
+    - Constraints & Limitations: Identify potential barriers or restrictions in moving from $A$ to $B$. These can be external (e.g., environmental factors) or internal (properties of $A$ or $B$).
+    - Resource and Information Analysis: What resources and information are required for the transition? This could be time, entity, factor, code language, software platform, unknowns, etc.
+    - External Influences: Consider socio-economic, political, or environmental factors (if applicable) that could influence the transition conditions.
+    - Creative/Heuristic Reasoning: Open your mind to multiple possible $C$'s, no matter how unconventional they might seem. Utilize analogies, metaphors, or brainstorming techniques to envision possible conditions or paths from $A$ to $B$.
+    - The conditions $C$ should be multiple but in one sentence. And each condition should be concerned with one aspect/entity.
+4. Entity/Label Recognition of Conditions ($C$):
+    - Identify and categorize entities of Conditions ($C$) such as the names, locations, dates, specific technical terms or contextual parameters that might be associated with events, innovations post-2022.
+    - The output of the entities/labels will be used as tags or labels for semantic similarity searches. The entities/labels may be the words, or phrases, each of them should contain valuable, high information entropy information, and should be independent.
+    - Ensure that the identified entities are formatted in a manner suitable for database indexing and retrieval. Organize the entities into categories, and combine the category with its instance into a continuous phrase, without using colons or other separators.
+    - Format these entities for database indexing: output the category rather than its instance/content into a continuous phrase. For example, instead of "Jan. 02", identify it as "Event time".
+5. Quality Assessment ($Q$):
+    - Efficiency: How efficient is the transition from $A$ to $B$, which measures the resources used versus the desired outcome?
+    - Effectiveness: Did the transition achieve the desired outcome or was the target state achieved as intended?
+    - Safety & Risks: Assess any risks associated with the transition and the measures to mitigate them.
+    - Feedback Mechanisms: Incorporate feedback loops to continuously monitor and adjust the quality of transition, making it more adaptive.
+6. Iterative Evaluation:
+    - Test & Refine: Based on the initially deduced conditions and assessed quality, iterate the process to refine and optimize the transition. This might involve tweaking conditions, employing different paths, or changing resources.
+    - Feedback Integration: Use feedback to make improvements and increase the quality of the transition.
+7. Real-world scenarios often present challenges that may not be captured by models and frameworks. While using the model, maintain an adaptive mindset:
+    - Scenario Exploration: Continuously imagine various possible scenarios, both positive and negative, to prepare for unexpected events.
+    - Flexibility: Be prepared to modify conditions ($C$) or alter the path/process ($L$) if unforeseen challenges arise.
+    - Feedback Integration: Rapidly integrate feedback from actual implementations to adjust the model's application, ensuring relevancy and effectiveness.
 
 ===== TASK =====
 Given the starting state $A$ and the target state $B$, assuming that a path $L$ always exists between $A$ and $B$, how can one deduce or identify the necessary conditions $C$ and the quality $Q$ of the transition?
@@ -137,6 +142,7 @@ Given the starting state $A$ and the target state $B$, assuming that a path $L$ 
 - Logical Deduction of Conditions ($C$) (multiple conditions can be deduced):
     condition <NUM>:
         <BLANK>.
+- Entity/Label Recognition of Conditions:\n[<BLANK>, <BLANK>, ...] (include square brackets)
 - Quality Assessment ($Q$) (do not use symbols):
     <BLANK>.
 - Iterative Evaluation:\n<BLANK>/None"""  # noqa: E501
@@ -171,9 +177,19 @@ Given the starting state $A$ and the target state $B$, assuming that a path $L$ 
             f"condition {i}":
             cdt.replace("<", "").replace(">", "").strip().strip('\n')
             for i, cdt in re.findall(
-                r"condition (\d+):(.+?)(?=condition|- Quality)", msg.content,
+                r"condition (\d+):(.+?)(?=condition|- Entity)", msg.content,
                 re.DOTALL)
         }
+
+        labels_str = [
+            label.replace("<", "").replace(">", "").strip().strip('\n')
+            for label in re.findall(
+                r"Entity/Label Recognition of Conditions:\n\[(.+?)\]",
+                msg.content, re.DOTALL)[0].split(",")
+        ][0]
+        labels = [
+            label.strip().strip('\"\'') for label in labels_str.split(", ")
+        ]
 
         quality = [
             q.replace("<", "").replace(">", "").strip().strip('\n')
@@ -185,6 +201,7 @@ Given the starting state $A$ and the target state $B$, assuming that a path $L$ 
         # Convert them into JSON format
         conditions_and_quality_json = {}
         conditions_and_quality_json["conditions"] = condistions_dict
+        conditions_and_quality_json["labels"] = labels
         conditions_and_quality_json["quality"] = quality
 
         return conditions_and_quality_json
