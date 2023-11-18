32c4b64086e14c90913ba712683b1113a1c040f6
Edmund Fahrenheit <Fischerprogram@gmail.com>
2023-11-18 09:00:58 -0500

    base openai api client tests complete

--- test/openai_api/test_base_openai_api_client.py
+++ test/openai_api/test_base_openai_api_client.py
@@ -1,1 +1,40 @@
-import pytest
+# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+# Licensed under the Apache License, Version 2.0 (the “License”);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+
+from typing import Union
+from camel.openai_api.base_openai_api_client import BaseOpenAIAPIClient
+
+def test_get_api_key():
+    client = BaseOpenAIAPIClient()
+    api_key = client._get_api_key()
+    assert isinstance(api_key, str)
+
+def test_set_api_key():
+    client = BaseOpenAIAPIClient()
+    api_key = 'my-api-key'
+    client._set_api_key(api_key)
+    assert client._get_api_key() == api_key
+
+def test_get_set_api_base():
+    client = BaseOpenAIAPIClient()
+    api_base = 'my-api-base'
+    client.set_api_base(api_base)
+    assert client.get_api_base() == api_base
+
+def test_get_set_organization():
+    client = BaseOpenAIAPIClient()
+    organization = 'my-organization'
+    client.set_organization(organization)
+    organization = client.get_organization()
+    assert isinstance(organization, str)
--- test/openai_api/test_openai_manager.py
+++ test/openai_api/test_openai_manager.py
@@ -1,1 +1,85 @@
 import pytest
+from camel.openai_api.openai_manager import OpenAIManager
+from openai.types.beta import Assistant, AssistantDeleted, assistant_create_params
+from openai.types.beta.assistants import AssistantFile, FileDeleteResponse
+from openai.pagination import SyncCursorPage
+from camel.types.enums import ModelType
+
+class TestOpenAIManager:
+    @pytest.fixture
+    def openai_manager(self):
+        return OpenAIManager()
+
+    def test_create_assistant(self, openai_manager):
+        name = "Test Assistant"
+        model = "gpt-3.5-turbo"
+        file_ids = ["file1", "file2"]
+        instructions = "Test instructions"
+        tools = [assistant_create_params.Tool(type="code_interpreter", name="Python")]
+        description = "Test assistant description"
+
+        assistant = openai_manager.create_assistant(name, model, file_ids, instructions, tools, description)
+
+        assert isinstance(assistant, Assistant)
+
+    def test_update_assistant(self, openai_manager):
+        name = "Test Assistant"
+        assistant_id = "assistant1"
+        description = "Updated description"
+        instructions = "Updated instructions"
+        file_ids = ["file1", "file2"]
+        tools = [assistant_create_params.Tool(type="retrieval", name="Search")]
+
+        assistant = openai_manager.update_assistant(name, assistant_id, description, instructions, file_ids, tools)
+
+        assert isinstance(assistant, Assistant)
+
+    def test_list_assistants(self, openai_manager):
+        assistants = openai_manager.list_assistants()
+
+        assert isinstance(assistants, SyncCursorPage)
+
+    def test_retrieve_assistant(self, openai_manager):
+        assistant_id = "assistant1"
+
+        assistant = openai_manager.retrieve_assitant(assistant_id)
+
+        assert isinstance(assistant, Assistant)
+
+    def test_delete_assistant(self, openai_manager):
+        assistant_id = "assistant1"
+
+        response = openai_manager.delete_assistant(assistant_id)
+
+        assert isinstance(response, AssistantDeleted)
+
+    def test_create_assistant_file(self, openai_manager):
+        file_id = "file1"
+        assistant_id = "assistant1"
+
+        assistant_file = openai_manager.create_assistant_file(file_id, assistant_id)
+
+        assert isinstance(assistant_file, AssistantFile)
+
+    def test_retrieve_assistant_file(self, openai_manager):
+        file_id = "file1"
+        assistant_id = "assistant1"
+
+        assistant_file = openai_manager.retrieve_assistant_file(file_id, assistant_id)
+
+        assert isinstance(assistant_file, AssistantFile)
+
+    def test_list_assistant_files(self, openai_manager):
+        assistant_id = "assistant1"
+
+        files = openai_manager.list_assistant_files(assistant_id)
+
+        assert isinstance(files, SyncCursorPage)
+
+    def test_delete_assistant_file(self, openai_manager):
+        file_id = "file1"
+        assistant_id = "assistant1"
+
+        response = openai_manager.delete_assistant_file(file_id, assistant_id)
+
+        assert isinstance(response, FileDeleteResponse)
