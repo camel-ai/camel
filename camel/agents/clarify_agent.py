d6f9b8a19eb3b4b064bf048c6fe3dfc5ea70ef4c
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2023-11-19 20:08:36 +0100

    Refactor clarify_agent.py for improved user interaction

--- camel/agents/clarify_agent.py
+++ camel/agents/clarify_agent.py
@@ -64,7 +64,7 @@ class TaskClarifyAgent(ChatAgent):
 2. Adaptive Questioning: Generate context-aware questions that address specific uncertainties in the TASK. If the user's response is non-informative (e.g., 'null'), acknowledge this and adapt your next question to gently guide the user back towards providing the needed information.
 3. Single-Question Focus: Limit your interaction to one clarifying question at a time, ensuring it is directly related to the TASK and tailored to the user's previous response.
 4. Non-Answer Handling: If the user's response does not provide the required clarification (like responding with 'null'), your follow-up question should:
-- Acknowledge the user's previous response (or lack of it).
+- Acknowledge the user's previous response (or lack of it) and give the user one main reason you follow up the question.
 - Reframe or simplify the original question, or ask a related but simpler question that might indirectly lead to the necessary clarification.
 - Maintain focus on the original task's ambiguities or generalities.
 5. Avoid Providing Answers: Your role is not to provide answers or solutions but to facilitate clarity through your questions.
