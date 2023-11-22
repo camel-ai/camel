d9b25aadeabd48dcdc5d57cef0fa8a1b6c5c7a6f
Tianqi Xu <40522713+dandansamax@users.noreply.github.com>
2023-11-22 17:33:03 +0300

    Update camel/types/enums.py
    Co-authored-by: Guohao Li <lightaime@gmail.com>

--- camel/types/enums.py
+++ camel/types/enums.py
@@ -44,13 +44,13 @@ class ModelType(Enum):
     @property
     def is_openai(self) -> bool:
         r"""Returns whether this type of models is an OpenAI-released model."""
-        return self.name in {
-            "GPT_3_5_TURBO",
-            "GPT_3_5_TURBO_16K",
-            "GPT_4",
-            "GPT_4_32K",
-            "GPT_4_TURBO",
-            "GPT_4_TURBO_VISION",
+        return self in {
+            ModelType.GPT_3_5_TURBO,
+            ModelType.GPT_3_5_TURBO_16K,
+            ModelType.GPT_4,
+            ModelType.GPT_4_32K,
+            ModelType.GPT_4_TURBO,
+            ModelType.GPT_4_TURBO_VISION,
         }
 
     @property
