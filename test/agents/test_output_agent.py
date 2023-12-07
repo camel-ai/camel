d513076ecc3a6ab07844955cb03fead00fe63b7e
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2023-12-07 20:14:48 +0100

    Refactor test_output_agent.py to generate detailed instruction

--- test/agents/test_output_agent.py
+++ test/agents/test_output_agent.py
@@ -11,34 +11,59 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+# # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+# # Licensed under the Apache License, Version 2.0 (the “License”);
+# # you may not use this file except in compliance with the License.
+# # You may obtain a copy of the License at
+# #
+# #     http://www.apache.org/licenses/LICENSE-2.0
+# #
+# # Unless required by applicable law or agreed to in writing, software
+# # distributed under the License is distributed on an “AS IS” BASIS,
+# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# # See the License for the specific language governing permissions and
+# # limitations under the License.
+# # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
 import pytest
+from mock import patch
 
+from camel.agents import ChatAgent
 from camel.agents.output_agent import OutputAgent
+from camel.configs import ChatGPTConfig
+from camel.messages import BaseMessage
+from camel.responses import ChatAgentResponse
+from camel.types import ModelType, RoleType
 
 
-@pytest.fixture
-def agent():
-    content = "Sample content for testing."
-    return OutputAgent(content)
+# Helper function to generate mock content
+def generate_mock_content():
+    return "Sample instruction for testing."
 
 
-def test_construct_task_prompt(agent):
-    messages = agent.construct_task_prompt()
-    expected_messages = [{
-        "role":
-        "system",
-        "content":
-        ("You are a helpful assistant to re-organize information "
-         "into a detailed instruction, below is the content for you:\n"
-         "Sample content for testing.")
-    }, {
-        "role":
-        "user",
-        "content":
-        ("Please extract the detailed action information from the "
-         "provided content, "
-         "make it useful for a human to follow the detailed instruction "
-         "step by step "
-         "to solve the task.")
-    }]
-    assert messages == expected_messages
+# The actual test case
+@pytest.mark.parametrize("model_type", [None, ModelType.GPT_3_5_TURBO])
+def test_generate_detailed_instruction(model_type):
+    mock_content = generate_mock_content()
+    mock_msg = BaseMessage(role_name="Output Agent",
+                           role_type=RoleType.ASSISTANT, meta_dict=None,
+                           content=mock_content)
+
+    # Construct the mock step function response
+    mock_step_response = ChatAgentResponse(msgs=[mock_msg], terminated=False,
+                                           info={})
+
+    # Setup the patch for the ChatAgent's step method
+    with patch.object(ChatAgent, 'step', return_value=mock_step_response):
+        content = "Sample content for testing."
+        model_config = ChatGPTConfig()
+
+        # Construct the output agent
+        output_agent = OutputAgent(model_type=model_type,
+                                   model_config=model_config)
+
+        # Generate the detailed instruction based on the mock step function
+        instruction = output_agent.generate_detailed_instruction(content)
+
+        expected_instruction = generate_mock_content()
+
+        assert instruction == expected_instruction
