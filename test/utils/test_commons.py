188cf216b73ec03a06aa79b425126dd0b47cb070
0xAC <n0_0ne@126.com>
2023-12-09 03:01:59 +0000

    Add testcases for `dependencies_required` and `api_keys_required` decorators.

--- test/utils/test_commons.py
+++ test/utils/test_commons.py
@@ -11,7 +11,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
-from camel.utils import get_task_list
+import os
+
+import pytest
+
+from camel.utils import api_keys_required, dependencies_required, get_task_list
 
 
 def test_get_task_list():
@@ -37,3 +41,41 @@ def test_get_task_list():
     assert isinstance(task_list, list)
     assert isinstance(task_list[0], str)
     assert len(task_list) == 1
+
+
+def test_dependencies_required(monkeypatch):
+
+    @dependencies_required('os')
+    def mock_dependencies_present():
+        return True
+
+    assert True if mock_dependencies_present() else False
+
+    @dependencies_required('some_module_not_exist')
+    def mock_dependencies_not_present():
+        return True
+
+    with pytest.raises(ImportError) as exc:
+        mock_dependencies_not_present()
+
+    assert "Missing required modules: some_module_not_exist" in str(exc.value)
+
+
+def test_api_keys_required():
+    os.environ['API_KEY_1'] = 'API_KEY_1_VALUE'
+    os.environ['API_KEY_2'] = 'API_KEY_2_VALUE'
+
+    @api_keys_required('API_KEY_1', 'API_KEY_2')
+    def mock_api_keys_exist():
+        return True
+
+    assert True if mock_api_keys_exist() else False
+
+    @api_keys_required('API_KEY_1', 'API_KEY_2', 'API_KEY_3')
+    def mock_api_keys_not_exist():
+        return True
+
+    with pytest.raises(ValueError) as exc:
+        mock_api_keys_not_exist()
+
+    assert "Missing API keys: API_KEY_3" in str(exc.value)
