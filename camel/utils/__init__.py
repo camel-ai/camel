b9ce699d9667efc3904ac34460b7fbae1b78b305
Jiahui Zhang <77677632+forever-ly@users.noreply.github.com>
2024-01-14 12:05:23 +0800

    Improve function call (#279) (#393)
    Co-authored-by: Tianqi Xu <tianqi.xu@kaust.edu.sa>
    Co-authored-by: Guohao Li <lightaime@gmail.com>
    Co-authored-by: lig <guohao.li@kaust.edu.sa>

--- camel/utils/__init__.py
+++ camel/utils/__init__.py
@@ -18,7 +18,6 @@ from .commons import (
     get_prompt_template_key_words,
     get_first_int,
     download_tasks,
-    parse_doc,
     get_task_list,
     check_server_running,
 )
@@ -30,17 +29,15 @@ from .token_counting import (
 )
 
 __all__ = [
-    'count_tokens_openai_chat_models',
     'openai_api_key_required',
     'print_text_animated',
     'get_prompt_template_key_words',
     'get_first_int',
     'download_tasks',
     'PythonInterpreter',
-    'parse_doc',
     'get_task_list',
-    'get_model_encoding',
     'check_server_running',
+    'get_model_encoding',
     'BaseTokenCounter',
     'OpenAITokenCounter',
     'OpenSourceTokenCounter',
