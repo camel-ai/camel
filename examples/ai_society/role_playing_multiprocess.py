43e3591a1ea4ba67b49288fc0c4a58ff3e843676
lig <guohao.li@kaust.edu.sa>
2024-01-15 07:13:41 +0300

    Rename and fix docstrings

--- examples/ai_society/role_playing_multiprocess.py
+++ examples/ai_society/role_playing_multiprocess.py
@@ -43,7 +43,7 @@ def generate_data(assistant_idx: int, assistant_role_name: str, user_idx: int,
             temperature=1.4)),
     )
 
-    input_assistant_msg = role_play_session.init_chat()
+    input_msg = role_play_session.init_chat()
 
     if verbose:
         print(Fore.GREEN + "AI Assistant sys message:\n"
@@ -93,8 +93,7 @@ def generate_data(assistant_idx: int, assistant_role_name: str, user_idx: int,
 
     while message_counter < max_num_messages:
 
-        assistant_response, user_response = role_play_session.step(
-            input_assistant_msg)
+        assistant_response, user_response = role_play_session.step(input_msg)
 
         # Condition 1: User terminates the chat
         if user_response.terminated and user_response.info is not None:
@@ -165,7 +164,7 @@ def generate_data(assistant_idx: int, assistant_role_name: str, user_idx: int,
         message_dict[
             f"message_{message_counter}"] = assistant_response.msg.to_dict()
 
-        input_assistant_msg = assistant_response.msg
+        input_msg = assistant_response.msg
 
     message_dict["num_messages"] = message_counter
 
