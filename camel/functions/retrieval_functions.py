86854cefd4bb577e7919ccff60881ef81ba9982c
Wendong <w3ndong.fan@gmail.com>
2024-05-05 21:53:56 +0800

    format fix

--- camel/functions/retrieval_functions.py
+++ camel/functions/retrieval_functions.py
@@ -12,37 +12,40 @@
 # limitations under the License.
 # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
 from typing import List, Union
+
 from camel.functions import OpenAIFunction
 from camel.retrievers import AutoRetriever
 from camel.types import StorageType
 
-def information_retrieval(query: str, content_input_paths: Union[str, List[str]]) -> str:
-    r"""Performs an auto retrieal for information. Given a query,
-    this function will retrieve the information from the remote vector storage,
-    and return the retrieved information back. It is useful for information
-    retrieve.
+
+def information_retrieval(
+    query: str, content_input_paths: Union[str, List[str]]
+) -> str:
+    r"""Retrieves information from a remote vector storage based on the specified query. This function connects to a remote vector storage system and retrieves relevant information by processing the input query. It is essential to use this function when the answer to a question requires external knowledge sources.
 
     Args:
-        query (string): Question you want to be answered.
+        query (str): The question or query for which an answer is required.
         content_input_paths (Union[str, List[str]]): Paths to local
             files or remote URLs.
 
     Returns:
-        str: Aggregated information retrieved in response to the query.
+        str: The information retrieved in response to the query, aggregated and formatted as a string.
 
     Example:
-        information_retrieval(query = "what is camel?",content_input_paths=["https://lablab.ai/t/camel-tutorial-building-communicative-agents-for-large-scale-language-model-exploration", "https://www.camel-ai.org/"])
+        # Retrieve information about CAMEL AI.
+        information_retrieval(query = "what is CAMEL AI?",
+                            content_input_paths="https://www.camel-ai.org/")
     """
     auto_retriever = AutoRetriever(
-            url_and_api_key=("Your Milvus URI","Your Milvus Token"),
-            storage_type=StorageType.MILVUS)
+        url_and_api_key=("Your Milvus URI", "Your Milvus Token"),
+        storage_type=StorageType.MILVUS,
+    )
 
     retrieved_info = auto_retriever.run_vector_retriever(
-        query=query,
-        content_input_paths=content_input_paths,
-        top_k=3
+        query=query, content_input_paths=content_input_paths, top_k=3
     )
     return retrieved_info
+
 
 # add the function to OpenAIFunction list
 RETRIEVAL_FUNCS: List[OpenAIFunction] = [
