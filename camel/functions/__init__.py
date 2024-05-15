ea6e8a8a97df35c49a6c06ad44d842c48c8ac520
Wendong <w3ndong.fan@gmail.com>
2024-05-15 15:14:58 +0800

    fix

--- camel/functions/__init__.py
+++ camel/functions/__init__.py
@@ -11,14 +11,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
-from .google_maps_function import MAP_FUNCS
-from .math_functions import MATH_FUNCS
-from .open_api_function import OPENAPI_FUNCS
+# ruff: noqa: I001
 from .openai_function import (
     OpenAIFunction,
     get_openai_function_schema,
     get_openai_tool_schema,
 )
+
+from .google_maps_function import MAP_FUNCS
+from .math_functions import MATH_FUNCS
+from .open_api_function import OPENAPI_FUNCS
 from .retrieval_functions import RETRIEVAL_FUNCS
 from .search_functions import SEARCH_FUNCS
 from .twitter_function import TWITTER_FUNCS
@@ -26,13 +28,13 @@ from .weather_functions import WEATHER_FUNCS
 
 __all__ = [
     'OpenAIFunction',
-    'get_openai_tool_schema',
     'get_openai_function_schema',
-    'MATH_FUNCS',
-    'SEARCH_FUNCS',
-    'WEATHER_FUNCS',
+    'get_openai_tool_schema',
     'MAP_FUNCS',
-    'TWITTER_FUNCS',
+    'MATH_FUNCS',
     'OPENAPI_FUNCS',
     'RETRIEVAL_FUNCS',
+    'SEARCH_FUNCS',
+    'TWITTER_FUNCS',
+    'WEATHER_FUNCS',
 ]
