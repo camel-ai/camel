11f2b1d23a391fb79ac16e6101df990bc4d74568
Wendong <w3ndong.fan@gmail.com>
2024-05-30 16:10:43 +0800

    use ModelType.STUB to pass test

--- examples/open_source_models/role_playing_with_open_source_model.py
+++ examples/open_source_models/role_playing_with_open_source_model.py
@@ -27,8 +27,8 @@ from camel.utils import print_text_animated
 def main(
     chat_turn_limit=50,
     model_platform=ModelPlatformType.OPENSOURCE,
-    model_type=ModelType.LLAMA_2,
-    model_path="TinyPixel/Llama-2-7B-bf16-sharded",
+    model_type=ModelType.STUB,
+    model_path="meta-llama/Llama-2-7b-chat-hf",
     server_url="http://localhost:8000/v1",
 ) -> None:
     task_prompt = "Develop a trading bot for the stock market"
