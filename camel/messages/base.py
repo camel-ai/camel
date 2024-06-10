0ddef8718a41dfbd942d8ab90efe68ce2f91162b
raywhoelse <112615585+raywhoelse@users.noreply.github.com>
2024-06-10 15:23:21 +0800

    format code

--- camel/messages/base.py
+++ camel/messages/base.py
@@ -362,7 +362,6 @@ class BaseMessage:
                 "content": self.content,
             }
 
-
     def to_openai_assistant_message(self) -> OpenAIAssistantMessage:
         r"""Converts the message to an :obj:`OpenAIAssistantMessage` object.
 
