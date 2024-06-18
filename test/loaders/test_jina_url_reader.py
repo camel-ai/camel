6a15b441c1abd00dc275aa2a74230ca209d87ee8
Isaac <whale3ye@gmail.com>
2024-06-17 20:30:59 -0500

    test(loaders): add test cases for jina url reader

--- test/loaders/test_jina_url_reader.py
+++ test/loaders/test_jina_url_reader.py
@@ -11,39 +11,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+import json
+
 import pytest
 
 from camel.loaders import JinaURLReader
 
 
 @pytest.fixture
-def jina_default_reader():
-    return JinaURLReader(timeout=100)
+def default_reader():
+    return JinaURLReader()
 
 
-def test_read_content_default(jina_default_reader: JinaURLReader):
-    test_url = "https://www.ign.com/articles/the-best-reviewed-games-of-2024"
-    content = jina_default_reader.read_content(test_url)
-    assert content is not None
-    print(content)
+@pytest.fixture
+def short_timeout_reader():
+    return JinaURLReader(timeout=3)
 
 
-def test_read_content_no_protocol():
-    # use the default return format
-    default_reader = JinaURLReader()
-    content = default_reader.read_content("www.example.com")
-    print(content)
+def test_read_content_default(default_reader: JinaURLReader):
+    test_url = "https://en.wikipedia.org/wiki/Hollow_Knight"
+    content = default_reader.read_content(test_url)
+    assert "Title: Hollow Knight" in content
 
 
-def test_read_content_invalid_url():
-    # use the default return format
-    default_reader = JinaURLReader()
-    content = default_reader.read_content("https://www.example.com")
-    print(content)
+def test_read_content_no_protocol(default_reader: JinaURLReader):
+    test_url = "en.wikipedia.org/wiki/Hollow_Knight"
+    content = default_reader.read_content(test_url)
+    assert "Title: Hollow Knight" in content
 
 
-def test_read_content_timeout():
-    default_reader = JinaURLReader(timeout=10)
+def test_read_content_markdown():
+    test_url = "https://en.wikipedia.org/wiki/Hollow_Knight"
+    markdown_reader = JinaURLReader(
+        return_format=JinaURLReader.ReturnFormat.MARKDOWN
+    )
+    content = markdown_reader.read_content(test_url)
+    assert "_Hollow Knight_" in content
+
+
+def test_read_content_html():
+    test_url = "https://en.wikipedia.org/wiki/Hollow_Knight"
+    html_reader = JinaURLReader(return_format=JinaURLReader.ReturnFormat.HTML)
+    content = html_reader.read_content(test_url)
+    assert "<title>Hollow Knight - Wikipedia</title>" in content
+
+
+def test_read_content_text():
+    test_url = "https://en.wikipedia.org/wiki/Hollow_Knight"
+    text_reader = JinaURLReader(return_format=JinaURLReader.ReturnFormat.TEXT)
+    content = text_reader.read_content(test_url)
+    assert "Hollow Knight" in content
+
+
+def test_read_content_json():
+    test_url = "https://en.wikipedia.org/wiki/Hollow_Knight"
+    json_reader = JinaURLReader(json_response=True)
+    content = json_reader.read_content(test_url)
+    content_json = json.loads(content)
+    assert "data" in content_json
+    assert "title" in content_json["data"]
+    assert "Hollow Knight" in content_json["data"]["title"]
+
+
+def test_read_content_invalid_url(short_timeout_reader: JinaURLReader):
+    invalid_url = "some_invalid_url"
+    with pytest.raises(Exception, match="timeout"):
+        short_timeout_reader.read_content(invalid_url)
+
+
+def test_read_content_timeout(short_timeout_reader: JinaURLReader):
     slow_url = "https://httpbin.org/delay/5"
-    content = default_reader.read_content(slow_url)
-    print(content)
+    with pytest.raises(Exception, match="timeout"):
+        short_timeout_reader.read_content(slow_url)
