c9812d1beaac86eede3e2fb160dd06a0de18b2c5
Caelum Forder <caelum119@gmail.com>
2024-07-12 18:51:32 +0100

    Change to twitter_toolkit for create tweet test
    Signed-off-by: Caelum Forder <caelum119@gmail.com>

--- test/toolkits/test_twitter_function.py
+++ test/toolkits/test_twitter_function.py
@@ -26,7 +26,7 @@ def twitter_toolkit():
 
 def test_create_tweet(monkeypatch, twitter_toolkit):
     # Get the logger for the module being tested
-    logger = get_logger('camel.functions.twitter_function')
+    logger = get_logger('camel.toolkits.twitter_toolkit')
     # Create a mock response object
     mock_response = MagicMock()
     mock_response.json.return_value = {
