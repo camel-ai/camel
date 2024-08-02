8f8b8bd6c031973333f3e904cf27ff6b4699bc4a
Wendong <w3ndong.fan@gmail.com>
2024-08-02 20:01:19 +0800

    update docstring

--- camel/storages/graph_storages/graph_element.py
+++ camel/storages/graph_storages/graph_element.py
@@ -24,48 +24,53 @@ except ImportError:
 
 
 class Node(BaseModel):
-    r"""Represents a node in a graph with associated properties."""
+    r"""Represents a node in a graph with associated properties.
+
+    Attributes:
+        id (Union[str, int]): A unique identifier for the node.
+        type (str):  The type of the relationship.
+        properties (dict): Additional properties and metadata associated with
+            the node.
+    """
 
     id: Union[str, int]
-    """A unique identifier for the node."""
-
     type: str = "Node"
-    """The type of the relationship."""
-
     properties: dict = Field(default_factory=dict)
-    """Additional properties and metadata associated with the node."""
 
 
 class Relationship(BaseModel):
-    r"""Represents a directed relationship between two nodes in a graph."""
+    r"""Represents a directed relationship between two nodes in a graph.
+
+    Attributes:
+        subj (Node): The subject/source node of the relationship.
+        obj (Node): The object/target node of the relationship.
+        type (str):  The type of the relationship.
+        properties (dict): Additional properties associated with the
+            relationship.
+    """
 
     subj: Node
-    """The subject/source node of the relationship."""
-
     obj: Node
-    """The object/target node of the relationship."""
-
     type: str = "Relationship"
-    """The type of the relationship."""
-
     properties: dict = Field(default_factory=dict)
-    """Additional properties associated with the relationship."""
 
 
 class GraphElement(BaseModel):
-    r"""A graph element with lists of nodes and relationships."""
+    r"""A graph element with lists of nodes and relationships.
 
-    # Allow arbitrary types for Element
+    Attributes:
+        nodes (List[Node]): A list of nodes in the graph.
+        relationships (List[Relationship]): A list of relationships in the
+            graph.
+        source (Element): The element from which the graph information is
+            derived.
+    """
+
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     nodes: List[Node]
-    """A list of nodes in the graph."""
-
     relationships: List[Relationship]
-    """A list of relationships in the graph."""
-
     source: Element
-    """The element from which the graph information is derived."""
 
     def __post_init__(self):
         if Element is None:
