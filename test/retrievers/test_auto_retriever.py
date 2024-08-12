03612e3d9f1acf2ef4a79eaee61ab19d37043646
Wendong-Fan <133094783+Wendong-Fan@users.noreply.github.com>
2024-08-12 15:37:20 +0800

    feat: Retrieval module support string as input (#817)

--- test/retrievers/test_auto_retriever.py
+++ test/retrievers/test_auto_retriever.py
@@ -70,14 +70,14 @@ def test_run_vector_retriever(auto_retriever):
     # Define mock data for testing
     query_related = "what is camel ai"
     query_unrealted = "unrelated query"
-    content_input_paths = "https://www.camel-ai.org/"
+    contents = "https://www.camel-ai.org/"
     top_k = 1
     similarity_threshold = 0.5
 
     # Test with query related to the content in mock data
     result_related = auto_retriever.run_vector_retriever(
         query_related,
-        content_input_paths,
+        contents,
         top_k,
         similarity_threshold,
         return_detailed_info=True,
@@ -94,7 +94,7 @@ def test_run_vector_retriever(auto_retriever):
 
     # Test with query unrelated to the content in mock data
     result_unrelated = auto_retriever.run_vector_retriever(
-        query_unrealted, content_input_paths, top_k, similarity_threshold
+        query_unrealted, contents, top_k, similarity_threshold
     )
 
     assert "No suitable information retrieved from" in result_unrelated
