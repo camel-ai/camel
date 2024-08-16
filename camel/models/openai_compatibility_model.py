b7d2fcab03f813c95a67109b960e5353107e7804
Lei Zhang <zhanglei@apache.org>
2024-08-16 16:37:45 +0800

    Update camel/models/openai_compatibility_model.py
    Co-authored-by: Wendong-Fan <133094783+Wendong-Fan@users.noreply.github.com>

--- camel/models/openai_compatibility_model.py
+++ camel/models/openai_compatibility_model.py
@@ -106,12 +106,16 @@ class OpenAICompatibilityModel:
 
     @property
     def token_limit(self) -> int:
-        r"""Returns the maximum token limit for a given model.
+        r"""Returns the maximum token limit for the given model.
 
         Returns:
             int: The maximum token limit for the given model.
         """
-        return (
-            self.model_config_dict.get("max_tokens")
-            or 256  # The default value of OpenAI is 256.
+        max_tokens = self.model_config_dict.get("max_tokens")
+        if isinstance(max_tokens, int):
+            return max_tokens
+        print(
+            "Must set `max_tokens` as an integer in `model_config_dict` when"
+            " setting up the model. Using 4096 as default value."
         )
+        return 4096
