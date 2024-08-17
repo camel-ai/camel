79b3860a1318b594530b15be855ed2d7caa54ffa
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-08-17 23:12:38 +0800

    feat: remove gpt-4-32k model

--- camel/types/enums.py
+++ camel/types/enums.py
@@ -26,7 +26,6 @@ class RoleType(Enum):
 class ModelType(Enum):
     GPT_3_5_TURBO = "gpt-3.5-turbo"
     GPT_4 = "gpt-4"
-    GPT_4_32K = "gpt-4-32k"
     GPT_4_TURBO = "gpt-4-turbo"
     GPT_4O = "gpt-4o"
     GPT_4O_MINI = "gpt-4o-mini"
