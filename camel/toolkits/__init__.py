7579d1e9740e89d14197a729612fac872eaf0427
Wendong <w3ndong.fan@gmail.com>
2024-09-27 19:33:30 +0800

    update toolkits init file

--- camel/toolkits/__init__.py
+++ camel/toolkits/__init__.py
@@ -19,23 +19,26 @@ from .openai_function import (
 )
 from .open_api_specs.security_config import openapi_security_config
 
-from .google_maps_toolkit import GoogleMapsToolkit
-from .math_toolkit import MathToolkit, MATH_FUNCS
-from .open_api_toolkit import OpenAPIToolkit
-from .retrieval_toolkit import RetrievalToolkit
-from .search_toolkit import SearchToolkit, SEARCH_FUNCS
-from .twitter_toolkit import TwitterToolkit
-from .weather_toolkit import WeatherToolkit, WEATHER_FUNCS
-from .slack_toolkit import SlackToolkit
-from .dalle_toolkit import DalleToolkit, DALLE_FUNCS
-from .linkedin_toolkit import LinkedInToolkit
-from .reddit_toolkit import RedditToolkit
 
+from .math_toolkit import MathToolkit, MATH_FUNCS
+from .search_toolkit import SearchToolkit, SEARCH_FUNCS
+from .weather_toolkit import WeatherToolkit, WEATHER_FUNCS
+from .dalle_toolkit import DalleToolkit, DALLE_FUNCS
+
+from .base import BaseToolkit
+from .google_maps_toolkit import GoogleMapsToolkit
 from .code_execution import CodeExecutionToolkit
 from .github_toolkit import GithubToolkit
 from .arxiv_toolkit import ArxivToolkit
+from .linkedin_toolkit import LinkedInToolkit
+from .reddit_toolkit import RedditToolkit
+from .slack_toolkit import SlackToolkit
+from .twitter_toolkit import TwitterToolkit
+from .open_api_toolkit import OpenAPIToolkit
+from .retrieval_toolkit import RetrievalToolkit
 
 __all__ = [
+    'BaseToolkit',
     'OpenAIFunction',
     'get_openai_function_schema',
     'get_openai_tool_schema',
