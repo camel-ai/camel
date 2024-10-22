0dea8059dbc0f1172eaf51638ad780f0579a6318
yaoxie220 <yx2845@columbia.edu>
2024-10-22 11:28:39 -0400

    chunkr integrated

--- camel/loaders/chunkr_reader.py
+++ camel/loaders/chunkr_reader.py
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+
 import json
 import logging
 import os
@@ -21,21 +22,20 @@ import requests
 
 from camel.utils import api_keys_required
 
-CHUNKR_ENDPOINT = "https://api.chunkr.ai/api/v1/task"
-
 logging.basicConfig(level=logging.INFO)
 
 
 class ChunkrReader:
     r"""Chunkr Reader for processing documents and returning content
     in various formats.
-
     Args:
         api_key (Optional[str], optional): The API key for Chunkr API. If not
             provided, it will be retrieved from the environment variable
-            `CHUNKR_API_KEY`. Defaults to None.
+            `CHUNKR_API_KEY`. (default: :obj:`None`)
+        url (Optional[str], optional): The url to the Chunkr service.
+            (default: :obj:`https://api.chunkr.ai/api/v1/task`)
         timeout (int, optional): The maximum time in seconds to wait for the
-            API responses. Defaults to 30.
+            API responses. (default: :obj:`30`)
         **kwargs (Any): Additional keyword arguments for request headers.
     """
 
@@ -43,10 +43,12 @@ class ChunkrReader:
     def __init__(
         self,
         api_key: Optional[str] = None,
+        url: Optional[str] = "https://api.chunkr.ai/api/v1/task",
         timeout: int = 30,
         **kwargs: Any,
     ) -> None:
-        api_key = self._api_key = api_key or os.getenv('CHUNKR_API_KEY')
+        self._api_key = api_key or os.getenv('CHUNKR_API_KEY')
+        self._url = os.getenv('CHUNKR_API_URL') or url
         self._headers = {
             "Authorization": f"{self._api_key}",
             **kwargs,
@@ -61,20 +63,16 @@ class ChunkrReader:
         target_chunk_length: str = "512",
     ) -> str:
         r"""Submits a file to the Chunkr API and returns the task ID.
-
         Args:
             file_path (str): The path to the file to be uploaded.
             model (str, optional): The model to be used for the task.
-                Defaults to 'Fast'.
+                (default: :obj:`Fast`)
             ocr_strategy (str, optional): The OCR strategy. Defaults to 'Auto'.
             target_chunk_length (str, optional): The target chunk length.
-                Defaults to '512'.
-
+                (default: :obj:`512`)
         Returns:
             str: The task ID.
         """
-        url_post = CHUNKR_ENDPOINT
-
         with open(file_path, 'rb') as file:
             files: dict[
                 str, Union[tuple[None, IO[bytes]], tuple[None, str]]
@@ -89,7 +87,7 @@ class ChunkrReader:
             }
             try:
                 response = requests.post(
-                    url_post,
+                    self._url, # type: ignore[arg-type]
                     headers=self._headers,
                     files=files,
                     timeout=self.timeout,
@@ -106,19 +104,24 @@ class ChunkrReader:
                 logging.error(f"Failed to submit task: {e}")
                 raise ValueError(f"Failed to submit task: {e}") from e
 
-    def get_task_output(self, task_id: str) -> str:
+    def get_task_output(self, task_id: str, max_retries: int = 5) -> str:
         r"""Polls the Chunkr API to check the task status and returns the task
         result.
-
         Args:
             task_id (str): The task ID to check the status for.
-
+            max_retries (int, optional): Maximum number of retry attempts. 
+                (default: :obj:`5`)
         Returns:
             str: The formatted task result in JSON format.
+        Raises:
+            ValueError: If the task status cannot be retrieved.
+            RuntimeError: If the maximum number of retries is reached without
+                a successful task completion.
         """
-        url_get = f"{CHUNKR_ENDPOINT}/{task_id}"
+        url_get = f"{self._url}/{task_id}"
+        attempts = 0
 
-        while True:
+        while attempts < max_retries:
             try:
                 response = requests.get(
                     url_get, headers=self._headers, timeout=self.timeout
@@ -138,14 +141,16 @@ class ChunkrReader:
                 logging.error(f"Failed to retrieve task status: {e}")
                 raise ValueError(f"Failed to retrieve task status: {e}") from e
 
+            attempts += 1
             time.sleep(5)
+
+        logging.error(f"Max retries reached for task {task_id}.")
+        raise RuntimeError(f"Max retries reached for task {task_id}.")
 
     def _pretty_print_response(self, response_json: dict) -> str:
         r"""Pretty prints the JSON response.
-
         Args:
             response_json (dict): The response JSON to pretty print.
-
         Returns:
             str: Formatted JSON as a string.
         """
