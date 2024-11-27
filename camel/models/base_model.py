dc11f3fe5d14b3137b31583e54ae8d0ffe906bc7
RomanPryima1 <roman.pryima@hygge.software>
2024-11-27 20:36:31 +0100

    [feature/model_manager_load_balancer] Add ModelManager to schedule calls between different model backend - implemented PR comments

--- camel/models/base_model.py
+++ camel/models/base_model.py
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
-import logging
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Optional, Union
 
