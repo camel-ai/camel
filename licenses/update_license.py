da44ecce182486d4bf41897b01c5bfb13ff6209e
koch3092 <108793340+koch3092@users.noreply.github.com>
2024-11-28 00:36:45 +0800

    fix: the Chinese characters in the license template into English … (#1231)
    Co-authored-by: Wendong <w3ndong.fan@gmail.com>

--- licenses/update_license.py
+++ licenses/update_license.py
@@ -1,16 +1,16 @@
-# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
-# Licensed under the Apache License, Version 2.0 (the “License”);
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an “AS IS” BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
 import os
 import re
 import sys
@@ -122,8 +122,8 @@ if __name__ == '__main__':
         directory_path = sys.argv[1]
         license_template_path = sys.argv[2]
 
-    start_line_start_with = "# =========== Copyright"
-    end_line_start_with = "# =========== Copyright"
+    start_line_start_with = "# ========= Copyright"
+    end_line_start_with = "# ========= Copyright"
     update_license_in_directory(
         directory_path,
         license_template_path,
