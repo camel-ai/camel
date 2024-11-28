2b41abc7f78a66bf004a9901b14c583e9afc8773
Wendong-Fan <133094783+Wendong-Fan@users.noreply.github.com>
2024-11-28 21:12:58 +0800

    fix: Support Text to BaseModel (#1241)

--- camel/schemas/openai_converter.py
+++ camel/schemas/openai_converter.py
@@ -11,19 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
-# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
-# Licensed under the Apache License, Version 2.0 (the “License”);
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an “AS IS” BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
 
 import os
 from typing import Any, Callable, Dict, Optional, Type, Union
@@ -47,8 +34,8 @@ DEFAULT_CONVERTER_PROMPTS = """
 
 
 class OpenAISchemaConverter(BaseConverter):
-    r"""OpenAISchemaConverter is a class that
-    converts a string or a function into a BaseModel schema.
+    r"""OpenAISchemaConverter is a class that converts a string or a function
+    into a BaseModel schema.
 
     Args:
         model_type (ModelType, optional): The model type to be used.
