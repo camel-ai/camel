ba1616cee3aebbd3f533be7ed65740ed564dab93
Wendong <w3ndong.fan@gmail.com>
2024-12-02 23:39:07 +0800

    pre commit fix

--- camel/messages/conversion/alpaca.py
+++ camel/messages/conversion/alpaca.py
@@ -1,16 +1,17 @@
-# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
-# Licensed under the Apache License, Version 2.0 (the “License”);
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an “AS IS” BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+
 import re
 
 from pydantic import BaseModel, Field, field_validator
@@ -38,8 +39,14 @@ class AlpacaItem(BaseModel):
 
     @field_validator('instruction', 'output')
     def no_section_markers(cls, value: str) -> str:
-        r"""Ensures fields don't contain section markers like '### Response:'"""
-        if '### Response' in value or '### Instruction' in value or '### Input' in value:
+        r"""Ensures fields don't contain section markers like '###
+        Response:'
+        """
+        if (
+            '### Response' in value
+            or '### Instruction' in value
+            or '### Input' in value
+        ):
             raise ValueError("Field cannot contain section markers")
         return value.strip()
 
