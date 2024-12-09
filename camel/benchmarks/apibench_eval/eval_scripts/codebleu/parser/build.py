acd676cbeeca00cf79413bd28a20cc509186cf07
Harry-QY <qy250@cam.ac.uk>
2024-12-09 10:37:22 +0000

    fix: Fix tree_sitter_import issue

--- camel/benchmarks/apibench_eval/eval_scripts/codebleu/parser/build.py
+++ camel/benchmarks/apibench_eval/eval_scripts/codebleu/parser/build.py
@@ -2,13 +2,17 @@
 # Licensed under the MIT license.
 
 from tree_sitter import Language, Parser
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+tree_sitter_python_path = os.path.join(current_dir, 'tree-sitter-python')
 
 Language.build_library(
   # Store the library in the `build` directory
   'my-languages.so',
 
   # Include one or more languages
-  [ 'tree-sitter-python',
+  [ tree_sitter_python_path,
   ]
 )
 
