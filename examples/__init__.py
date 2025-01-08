cbe681eea94179d1d087380969e2a6ed05ab33c2
Wendong <w3ndong.fan@gmail.com>
2025-01-09 00:27:19 +0800

    fix

--- examples/__init__.py
+++ examples/__init__.py
@@ -11,19 +11,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
-
-from camel.agents import ChatAgent
-from camel.benchmarks import RAGBenchBenchmark
-from camel.retrievers import AutoRetriever
-
-assistant_sys_msg = """You are a helpful assistant to answer question,
-         I will give you the Original Query and Retrieved Context,
-        answer the Original Query based on the Retrieved Context,
-        if you can't answer the question just say I don't know."""
-agent = ChatAgent(assistant_sys_msg)
-auto_retriever = AutoRetriever()
-
-benchmark = RAGBenchBenchmark(subset="hotpotqa", split="test")
-benchmark.download()
-results = benchmark.run(agent, auto_retriever)
-print(results)
