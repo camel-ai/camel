34d22c2bdeb50739c49b81bab099617ce4b288bf
Aaron617 <1170813560@qq.com>
2025-01-10 13:16:43 +0800

    update unit test for video toolkit
    @harryeqs

--- camel/toolkits/video_toolkit.py
+++ camel/toolkits/video_toolkit.py
@@ -209,3 +209,42 @@ class VideoDownloaderToolkit(BaseToolkit):
             FunctionTool(self.get_video_bytes),
             FunctionTool(self.get_video_screenshots),
         ]
+
+
+
+# @mengkang: Unit Test for video toolkit 
+if __name__ == "__main__":
+    import pytest
+    from camel.toolkits import VideoToolkit
+
+
+    @pytest.fixture
+    def video_toolkit():
+        return VideoToolkit()
+
+
+    def test_video_1(video_toolkit):
+
+        video_path = "https://www.youtube.com/watch?v=L1vXCYZAYYM"
+        question = "what is the highest number of bird species to be on camera simultaneously? Please answer with only the number."
+
+        res = video_toolkit.ask_question_about_video(video_path, question)
+        assert res == "3"
+
+
+    def test_video_2(video_toolkit):
+
+        video_path = "https://www.youtube.com/watch?v=1htKBjuUWec"
+        question = "What does Teal'c say in response to the question \"Isn't that hot?\" Please answer with the exact words or phrase."
+
+        res = video_toolkit.ask_question_about_video(video_path, question)
+        assert "extremely" in res.lower()
+
+
+    def test_video_3(video_toolkit):
+
+        video_path = "https://www.youtube.com/watch?v=2Njmx-UuU3M"
+        question = "What species of bird is featured in the video? Please answer with exactly the name of the species without any additional text."
+
+        res = video_toolkit.ask_question_about_video(video_path, question)
+        assert res.lower == "rockhopper penguin"
