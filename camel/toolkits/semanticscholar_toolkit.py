cfd7450a5b04a476b9c3f4599acc55d3a5c80a63
任信行 <renxinxing@renxinxingdeMacBook-Pro.local>
2025-01-25 21:47:46 +0800

    Re-foramtted the toolkits file

--- camel/toolkits/semanticscholar_toolkit.py
+++ camel/toolkits/semanticscholar_toolkit.py
@@ -1,8 +1,24 @@
-from camel.toolkits import FunctionTool
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+
+from typing import List 
+import requests  
+import json
+
+from camel.toolkits import FunctionTool  
 from camel.toolkits.base import BaseToolkit
 
-import requests
-import json
 
 class SemanticScholarToolkit(BaseToolkit):
     """A toolkit for interacting with the Semantic Scholar
