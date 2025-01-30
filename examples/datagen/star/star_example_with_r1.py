61afbc5b41f840fc4a15df49acfa2e48ee885a73
Wendong <w3ndong.fan@gmail.com>
2025-01-30 12:11:58 +0800

    update

--- examples/datagen/star/star_example_with_r1.py
+++ examples/datagen/star/star_example_with_r1.py
@@ -41,7 +41,7 @@ def main():
     start_time = time.time()
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    problems_path = os.path.join(current_dir, 'input_problems.json')
+    problems_path = os.path.join(current_dir, 'math_500_dataset.json')
     output_path = os.path.join(current_dir, 'star_r1_output.json')
 
     # Load problems from JSON file
