f4112463534e43c593f13dceb8a99fe84198b952
Rida KEJJI <rida.kejji@artefact.com>
2025-02-04 14:57:52 +0100

    test: tested anthropic model run

--- test/models/test_anthropic_model.py
+++ test/models/test_anthropic_model.py
@@ -11,29 +11,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+"""
+please set the below os environment variable:
+export ANTHROPIC_API_KEY="xxx"
+"""
+
 import re
+from types import SimpleNamespace
+from unittest.mock import MagicMock, patch
 
 import pytest
+from anthropic import NOT_GIVEN
 
 from camel.configs import AnthropicConfig
 from camel.models import AnthropicModel
-from camel.types import ModelType
+from camel.types import ChatCompletion, ModelType
 from camel.utils import AnthropicTokenCounter
+
+anthropic_model_response = SimpleNamespace(
+    id="msg_123456",
+    type="message",
+    role="assistant",
+    model="claude-2.1",
+    content=[SimpleNamespace(text="The capital city of Morocco is Rabat")],
+    stop_reason="stop reason",
+)
+system_role_message = {
+    "role": "system",
+    "content": "Answer the user's question with 6 words maximum",
+}
+user_role_message = {
+    "role": "user",
+    "content": "The capital of morocco is Rabat",
+}
+model_types = [
+    ModelType.CLAUDE_INSTANT_1_2,
+    ModelType.CLAUDE_2_0,
+    ModelType.CLAUDE_2_1,
+    ModelType.CLAUDE_3_OPUS,
+    ModelType.CLAUDE_3_SONNET,
+    ModelType.CLAUDE_3_HAIKU,
+    ModelType.CLAUDE_3_5_SONNET,
+    ModelType.CLAUDE_3_5_HAIKU,
+]
 
 
 @pytest.mark.model_backend
 @pytest.mark.parametrize(
     "model_type",
-    [
-        ModelType.CLAUDE_INSTANT_1_2,
-        ModelType.CLAUDE_2_0,
-        ModelType.CLAUDE_2_1,
-        ModelType.CLAUDE_3_OPUS,
-        ModelType.CLAUDE_3_SONNET,
-        ModelType.CLAUDE_3_HAIKU,
-        ModelType.CLAUDE_3_5_SONNET,
-        ModelType.CLAUDE_3_5_HAIKU,
-    ],
+    model_types,
 )
 def test_anthropic_model(model_type: ModelType):
     model = AnthropicModel(model_type)
@@ -59,3 +85,73 @@ def test_anthropic_model_unexpected_argument():
         ),
     ):
         _ = AnthropicModel(model_type, model_config_dict)
+
+
+@pytest.mark.model_backend
+@pytest.mark.parametrize(
+    "model_type",
+    model_types,
+)
+@patch("anthropic.Anthropic")
+def test_anthropic_run(mock_anthropic, model_type: ModelType):
+    # Mock the Anthropic create client function
+    mock_anthropic_client = MagicMock()
+    mock_anthropic.return_value = mock_anthropic_client
+    mock_anthropic_client.messages.create.return_value = (
+        anthropic_model_response
+    )
+
+    model = AnthropicModel(model_type)
+    model_inference = model.run(
+        messages=[system_role_message, user_role_message]  # type: ignore[list-item]
+    )
+    mock_anthropic_client.messages.create.assert_called_once_with(
+        model=model_type,
+        system=system_role_message["content"],
+        messages=[user_role_message],
+        max_tokens=8192,
+        temperature=1,
+        top_p=0.7,
+        top_k=5,
+        stream=False,
+    )
+    assert isinstance(model_inference, ChatCompletion)
+    assert model_inference.choices[0].message.role == "assistant"
+    assert (
+        model_inference.choices[0].message.content
+        == anthropic_model_response.content[0].text
+    )
+
+
+@pytest.mark.model_backend
+@pytest.mark.parametrize(
+    "model_type",
+    model_types,
+)
+@patch("anthropic.Anthropic")
+def test_anthropic_run_without_sys_msg(mock_anthropic, model_type: ModelType):
+    # Mock the Anthropic create client function
+    mock_anthropic_client = MagicMock()
+    mock_anthropic.return_value = mock_anthropic_client
+    mock_anthropic_client.messages.create.return_value = (
+        anthropic_model_response
+    )
+
+    model = AnthropicModel(model_type)
+    model_inference = model.run(messages=[user_role_message])  # type: ignore[list-item]
+    mock_anthropic_client.messages.create.assert_called_once_with(
+        model=model_type,
+        system=NOT_GIVEN,
+        messages=[user_role_message],
+        max_tokens=8192,
+        temperature=1,
+        top_p=0.7,
+        top_k=5,
+        stream=False,
+    )
+    assert isinstance(model_inference, ChatCompletion)
+    assert model_inference.choices[0].message.role == "assistant"
+    assert (
+        model_inference.choices[0].message.content
+        == anthropic_model_response.content[0].text
+    )
