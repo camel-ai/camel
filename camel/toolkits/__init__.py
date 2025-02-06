089626cd09aab52f086368a079ce9be7aa0cd31c
Wendong-Fan <133094783+Wendong-Fan@users.noreply.github.com>
2025-02-07 05:25:29 +0800

    enhance: Semantic Scholar (#1562)

--- camel/toolkits/__init__.py
+++ camel/toolkits/__init__.py
@@ -45,7 +45,7 @@ from .human_toolkit import HumanToolkit
 from .stripe_toolkit import StripeToolkit
 from .video_toolkit import VideoDownloaderToolkit
 from .dappier_toolkit import DappierToolkit
-from .semanticscholar_toolkit import SemanticScholarToolkit
+from .semantic_scholar_toolkit import SemanticScholarToolkit
 
 __all__ = [
     'BaseToolkit',
