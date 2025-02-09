bf95201ac2a7737c6389c0446c255c184db4f7ce
Wendong <w3ndong.fan@gmail.com>
2025-02-09 13:12:52 +0800

    remove ragas dependency

--- camel/benchmarks/ragbench.py
+++ camel/benchmarks/ragbench.py
@@ -188,7 +188,7 @@ def ragas_evaluate_dataset(
     Returns:
         Dataset: Dataset with added evaluation metrics.
     """
-    from ragas import evaluate
+    from ragas import evaluate  # type: ignore[import-untyped]
     from ragas.metrics import (  # type: ignore[import-untyped]
         context_relevancy,
         faithfulness,
