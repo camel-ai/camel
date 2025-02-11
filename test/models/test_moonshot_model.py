88e7d997180cd9252b2c5acf4d0b2c42be0bd9aa
Rida KEJJI <rida.kejji@artefact.com>
2025-02-11 10:36:03 +0100

    test: tested moonshot_model

--- test/models/test_moonshot_model.py
+++ test/models/test_moonshot_model.py
@@ -12,7 +12,13 @@
 # limitations under the License.
 # ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
 
+"""
+please set the below os environment:
+export MOONSHOT_API_KEY="xx"
+"""
+
 import re
+from unittest.mock import MagicMock, patch
 
 import pytest
 
@@ -20,15 +26,23 @@ from camel.configs import MoonshotConfig
 from camel.models import MoonshotModel
 from camel.types import ModelType
 
+model_types = [
+    ModelType.MOONSHOT_V1_8K,
+    ModelType.MOONSHOT_V1_32K,
+    ModelType.MOONSHOT_V1_128K,
+]
+
+
+user_role_message = {
+    "role": "user",
+    "content": "How fast can you solve a math problem ?",
+}
+
 
 @pytest.mark.model_backend
 @pytest.mark.parametrize(
     "model_type",
-    [
-        ModelType.MOONSHOT_V1_8K,
-        ModelType.MOONSHOT_V1_32K,
-        ModelType.MOONSHOT_V1_128K,
-    ],
+    model_types,
 )
 def test_moonshot_model(model_type: ModelType):
     model = MoonshotModel(model_type)
@@ -36,6 +50,39 @@ def test_moonshot_model(model_type: ModelType):
     assert model.model_config_dict == MoonshotConfig().as_dict()
     assert isinstance(model.model_type.value_for_tiktoken, str)
     assert isinstance(model.model_type.token_limit, int)
+
+
+@pytest.mark.model_backend
+@pytest.mark.parametrize(
+    "model_type",
+    model_types,
+)
+@patch("camel.models.moonshot_model.OpenAI")
+def test_internlm_model_run(mock_openai, model_type: ModelType):
+    # Mock the client creation function OpenAI
+    mock_openai_client = MagicMock()
+    mock_openai.return_value = mock_openai_client
+    mock_openai_client.chat.completions.create.return_value = None
+    model = MoonshotModel(model_type)
+    model.run([user_role_message])  # type: ignore[list-item]
+    mock_openai_client.chat.completions.create.assert_called_once_with(
+        messages=[
+            {
+                'role': 'user',
+                'content': 'How fast can you solve a math problem ?',
+            }
+        ],
+        model=model_type,
+        tools=None,
+        temperature=0.3,
+        max_tokens=None,
+        stream=False,
+        top_p=1.0,
+        n=1,
+        presence_penalty=0.0,
+        frequency_penalty=0.0,
+        stop=None,
+    )
 
 
 @pytest.mark.model_backend
