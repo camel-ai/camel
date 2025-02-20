fa3d9f3341974e58e0200a258f7def944259bbf4
Isaac Jin <whale3ye@gmail.com>
2025-02-19 23:05:36 -0600

    fix: Backend model tool call (#1624)
    Co-authored-by: Wendong-Fan <133094783+Wendong-Fan@users.noreply.github.com>

--- camel/models/_utils.py
+++ camel/models/_utils.py
@@ -21,7 +21,7 @@ from camel.messages import OpenAIMessage
 
 def try_modify_message_with_format(
     message: OpenAIMessage,
-    response_format: Optional[Type[BaseModel]] = None,
+    response_format: Optional[Type[BaseModel]],
 ) -> None:
     r"""Modifies the content of the message to include the instruction of using
     the response format.
@@ -51,7 +51,7 @@ def try_modify_message_with_format(
         
         Please generate a JSON response adhering to the following JSON schema:
         {json_schema}
-        Make sure the JSON response is valid and matches the EXACT structure defined in the schema. Your result should only be a valid json object, without any other text or comments.
+        Make sure the JSON response is valid and matches the EXACT structure defined in the schema. Your result should ONLY be a valid json object, WITHOUT ANY OTHER TEXT OR COMMENTS.
         """  # noqa: E501
     )
     message["content"] = updated_prompt
