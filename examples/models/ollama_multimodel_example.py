f49fa31ecc497032fd781e6bf1cbedfd2c151336
zjrwtx <3038880699@qq.com>
2025-03-02 12:23:06 +0800

    add ollama multimodal model example

--- examples/models/ollama_multimodel_example.py
+++ examples/models/ollama_multimodel_example.py
@@ -11,106 +11,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+from io import BytesIO
 
-from pydantic import BaseModel
+import requests
+from PIL import Image
 
 from camel.agents import ChatAgent
+from camel.messages import BaseMessage
 from camel.models import ModelFactory
 from camel.types import ModelPlatformType
 
-ollama_model = ModelFactory.create(
+model = ModelFactory.create(
     model_platform=ModelPlatformType.OLLAMA,
-    model_type="llama3.2",
+    model_type="llava-phi3",
     model_config_dict={"temperature": 0.4},
 )
 
-assistant_sys_msg = "You are a helpful assistant."
+agent = ChatAgent(system_message="""You are a assistant""", model=model)
 
-agent = ChatAgent(assistant_sys_msg, model=ollama_model, token_limit=4096)
+# URL of the image
+url = "https://raw.githubusercontent.com/camel-ai/camel/master/misc/logo_light.png"
+response = requests.get(url)
+image = Image.open(BytesIO(response.content))
 
-user_msg = """Say hi to CAMEL AI, one open-source community
-    dedicated to the study of autonomous and communicative agents."""
-
-assistant_response = agent.step(user_msg)
-print(assistant_response.msg.content)
-
-"""
-===============================================================================
-Ollama server started on http://localhost:11434/v1 for mistral model
-
-Hello CAMEL AI community!
-
-It's great to connect with such a fascinating group of individuals passionate 
-about autonomous and communicative agents. Your dedication to advancing 
-knowledge in this field is truly commendable.
-
-I'm here to help answer any questions, provide information, or engage in 
-discussions related to AI, machine learning, and autonomous systems. Feel free 
-to ask me anything!
-
-By the way, what topics would you like to explore within the realm of 
-autonomous and communicative agents?
-===============================================================================
-"""
-
-
-class Pet(BaseModel):
-    name: str
-    animal: str
-    age: int
-    color: str | None
-    favorite_toy: str | None
-
-
-class PetList(BaseModel):
-    pets: list[Pet]
-
-
-ollama_model = ModelFactory.create(
-    model_platform=ModelPlatformType.OLLAMA,
-    model_type="llama3.2",
-    # Ensure using ollama verison >= 0.5.1 to use structured output feature
-    model_config_dict={"temperature": 0, "response_format": PetList},
+context = "what's in the image?"
+message = BaseMessage.make_user_message(
+    role_name="user", content=context, image_list=[image]
 )
 
-assistant_sys_msg = "You are a helpful assistant."
-
-agent = ChatAgent(assistant_sys_msg, model=ollama_model, token_limit=4096)
-
-user_msg = """I have two pets.A cat named Luna who is 5 years old and loves
-            playing with yarn. She has grey fur. I also have a 2 year old
-            black cat named Loki who loves tennis balls."""
-
-assistant_response = agent.step(user_msg)
-print(assistant_response.msg.content)
-print(assistant_response.msg.parsed)
-
-"""
-===========================================================================
-[{'role': 'system', 'content': 'You are a helpful assistant.'}, {'role':
-'user', 'content': 'I have two pets.A cat named Luna who is 5 years old
-and loves playing with yarn. She has grey fur.I also have a 2 year old 
-black cat named Loki who loves tennis balls.'}]
-{ "pets": [
-    {
-        "age": 5,
-        "animal": "cat",
-        "color": "grey",
-        "favorite_toy": "yarn"
-    ,
-    "name": "Luna"
-},
-{
-    "age": 2,
-    "animal": "cat",
-    "color": "black",
-    "favorite_toy": "tennis balls"
-    ,
-    "name": "Loki"
-}]}
-
-pets=[Pet(name='Luna', animal='cat', age=5, color='grey',
-favorite_toy='yarn'), Pet(name='Loki', animal='cat', age=2,
-color='black', favorite_toy='tennis balls')]
-===========================================================================
-"""
+response = agent.step(message).msgs[0]
+print(response)
