185c56eef965a7d11342b312097e8c0b6b8877ce
zhanglei <zhanglei@apache.org>
2025-03-18 08:52:06 +0800

    feat(agents): implement conditional message recording in chat agent

--- test/agents/test_chat_agent.py
+++ test/agents/test_chat_agent.py
@@ -404,6 +404,77 @@ def test_chat_agent_messages_window():
 
 
 @pytest.mark.model_backend
+def test_chat_agent_record_messages_condition():
+    system_msg = BaseMessage(
+        role_name="assistant",
+        role_type=RoleType.ASSISTANT,
+        meta_dict=None,
+        content="You are a help assistant.",
+    )
+    assistant = ChatAgent(
+        system_message=system_msg,
+        message_window_size=20,
+    )
+
+    user_msg = BaseMessage(
+        role_name="User",
+        role_type=RoleType.USER,
+        meta_dict=dict(),
+        content="Tell me a joke.",
+    )
+
+    assistant.step(user_msg)
+    openai_messages, _ = assistant.memory.get_context()
+    assert len(openai_messages) == 3
+
+    # if record_message_condition is False, the message will not be recorded
+    assistant.step(
+        user_msg, record_message_condition=lambda input_msg, output_msg: False
+    )
+    openai_messages, _ = assistant.memory.get_context()
+    assert len(openai_messages) == 3
+
+
+@pytest.mark.model_backend
+def test_chat_agent_record_messages_condition_with_tools():
+    system_msg = BaseMessage(
+        role_name="assistant",
+        role_type=RoleType.ASSISTANT,
+        meta_dict=None,
+        content="You are a help assistant.",
+    )
+    assistant = ChatAgent(
+        system_message=system_msg,
+        message_window_size=20,
+        tools=MathToolkit().get_tools(),
+    )
+
+    user_msg = BaseMessage(
+        role_name="User",
+        role_type=RoleType.USER,
+        meta_dict=dict(),
+        content="1+2=?",
+    )
+
+    assistant.step(user_msg)
+    openai_messages, _ = assistant.memory.get_context()
+    assert len(openai_messages) == 5
+
+    # if record_message_condition is False, the message will not be recorded
+    user_msg = BaseMessage(
+        role_name="User",
+        role_type=RoleType.USER,
+        meta_dict=dict(),
+        content="4/5=?",
+    )
+    assistant.step(
+        user_msg, record_message_condition=lambda input_msg, output_msg: False
+    )
+    openai_messages, _ = assistant.memory.get_context()
+    assert len(openai_messages) == 5
+
+
+@pytest.mark.model_backend
 def test_chat_agent_step_exceed_token_number(step_call_count=3):
     system_msg = BaseMessage(
         role_name="assistant",
