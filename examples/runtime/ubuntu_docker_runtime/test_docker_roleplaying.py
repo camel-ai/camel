6f855dd7751e130e34221d4e43d43ad2dcff61c2
jjyaoao <jjyaoao@126.com>
2025-03-22 19:51:00 +0800

    update new readme for ubuntu docker runtime

--- examples/runtime/ubuntu_docker_runtime/test_docker_roleplaying.py
+++ examples/runtime/ubuntu_docker_runtime/test_docker_roleplaying.py
@@ -27,7 +27,7 @@ model = ModelFactory.create(
     model_platform=ModelPlatformType.OPENAI_COMPATIBLE_MODEL,
     model_type="Qwen/Qwen2.5-72B-Instruct",
     url='https://api-inference.modelscope.cn/v1/',
-    api_key="api",
+    api_key="api-key",
 )
 
 
