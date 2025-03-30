d64df9b41e5bfe894da80fcedbbada4c564a966a
koch3092 <developer@dorakoch.com>
2025-03-30 18:06:13 +0800

    feat: add reddit toolkit MCP server example [Not Tested]

---
+++ examples/mcp/toolkits/reddit_toolkit/reddit_toolkit.py
@@ -0,0 +1,35 @@
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+import argparse
+import sys
+
+from camel.toolkits import RedditToolkit
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run Reddit Toolkit with MCP server mode.",
+        usage=f"python {sys.argv[0]} [--mode MODE]",
+    )
+    parser.add_argument(
+        "--mode",
+        choices=["stdio", "sse"],
+        default="stdio",
+        help="MCP server mode (default: 'stdio')",
+    )
+
+    args = parser.parse_args()
+
+    math_toolkit = RedditToolkit()
+
+    math_toolkit.mcp.run(args.mode)
