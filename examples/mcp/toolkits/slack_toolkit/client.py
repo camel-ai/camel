4f4bd14a464651d33adf3785a1fe9b3e261c323f
koch3092 <developer@dorakoch.com>
2025-03-30 19:00:21 +0800

    feat: add slack toolkit MCP server example [Not Tested]

---
+++ examples/mcp/toolkits/slack_toolkit/client.py
@@ -0,0 +1,61 @@
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+import asyncio
+from typing import TYPE_CHECKING, Union
+
+from mcp.types import CallToolResult
+
+from camel.toolkits.mcp_toolkit import MCPToolkit, _MCPServer
+
+if TYPE_CHECKING:
+    from mcp import ListToolsResult
+
+
+async def run_example():
+    mcp_toolkit = MCPToolkit(config_path="./mcp_servers_config.json")
+
+    await mcp_toolkit.connect()
+
+    # call the server to list the available tools
+    mcp_client: _MCPServer = mcp_toolkit.servers[0]
+    res: Union[str, "ListToolsResult"] = await mcp_client.list_mcp_tools()
+    if isinstance(res, str):
+        raise Exception(res)
+
+    tools = [tool.name for tool in res.tools]
+    print(tools)
+    """
+===============================================================================
+['create_slack_channel', 'join_slack_channel', 'leave_slack_channel', 
+'get_slack_channel_information', 'get_slack_channel_message', 
+'send_slack_message', 'delete_slack_message']
+===============================================================================
+    """
+
+    res: CallToolResult = await mcp_client.session.call_tool(
+        "get_slack_channel_information", {}
+    )
+    print(res.content[0].text)
+
+    """
+===============================================================================
+[Not Tested]
+===============================================================================    
+    """
+
+    await mcp_toolkit.disconnect()
+
+
+if __name__ == "__main__":
+    asyncio.run(run_example())
