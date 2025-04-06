8b5307027d1349793ff5ffd6a214cbde90d69082
Wendong <w3ndong.fan@gmail.com>
2025-04-06 12:18:43 +0800

    fix: Vllm tool call schema remove additionalProperties

--- camel/models/vllm_model.py
+++ camel/models/vllm_model.py
@@ -162,6 +162,14 @@ class VLLMModel(BaseModelBackend):
         if response_format:
             kwargs["response_format"] = {"type": "json_object"}
 
+        # Remove additionalProperties from each tool's function parameters
+        if tools and "tools" in kwargs:
+            for tool in kwargs["tools"]:
+                if "function" in tool and "parameters" in tool["function"]:
+                    tool["function"]["parameters"].pop(
+                        "additionalProperties", None
+                    )
+
         response = await self._async_client.chat.completions.create(
             messages=messages,
             model=self.model_type,
@@ -196,6 +204,14 @@ class VLLMModel(BaseModelBackend):
             kwargs["tools"] = tools
         if response_format:
             kwargs["response_format"] = {"type": "json_object"}
+
+        # Remove additionalProperties from each tool's function parameters
+        if tools and "tools" in kwargs:
+            for tool in kwargs["tools"]:
+                if "function" in tool and "parameters" in tool["function"]:
+                    tool["function"]["parameters"].pop(
+                        "additionalProperties", None
+                    )
 
         response = self._client.chat.completions.create(
             messages=messages,
