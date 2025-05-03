14c9586311ac2db447348faca6a5b1b90e6bed34
Xiangkai Zeng <xiangkaiz@klavis.ai>
2025-05-03 02:45:49 -0700

    feat: Add Klavis toolkit. (#2309)
    Co-authored-by: Wendong-Fan <133094783+Wendong-Fan@users.noreply.github.com>

--- camel/toolkits/__init__.py
+++ camel/toolkits/__init__.py
@@ -68,6 +68,7 @@ from .pyautogui_toolkit import PyAutoGUIToolkit
 from .openai_agent_toolkit import OpenAIAgentToolkit
 from .searxng_toolkit import SearxNGToolkit
 from .jina_reranker_toolkit import JinaRerankerToolkit
+from .klavis_toolkit import KlavisToolkit
 
 
 __all__ = [
@@ -124,4 +125,5 @@ __all__ = [
     'OpenAIAgentToolkit',
     'SearxNGToolkit',
     'JinaRerankerToolkit',
+    'KlavisToolkit',
 ]
