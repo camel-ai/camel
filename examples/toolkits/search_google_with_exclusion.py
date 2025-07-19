6f62644b8fb5777a92657456bc2784b320a1b6cf
boerz-coding <boerzhang@g.harvard.edu>
2025-07-19 16:27:16 -0500

    feat: allow search_google toolkit to exclude specified domains specified by user

---
+++ examples/toolkits/search_google_with_exclusion.py
@@ -0,0 +1,39 @@
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
+from camel.toolkits import SearchToolkit
+
+# This example demonstrates how to use the SearchToolkit to
+# perform a Google search while excluding certain domains from the results.
+
+# Google search API Key and Search Engine ID are required to run this example
+# API Key: https://developers.google.com/custom-search/v1/overview
+# Search Engine ID: https://cse.google.com/cse/all
+
+
+toolkit = SearchToolkit(number_of_result_pages=3)
+search_result = toolkit.search_google(query="Python Wikipedia")
+print("Search results for 'Python Wikipedia':\n")
+for result in search_result:
+    print(result['url'])
+search_result = toolkit.search_google(
+    query="Python Wikipedia",
+    exclude_domains=[
+        "wikipedia.org",
+        "wikipedia-api.readthedocs.io",
+        "wikipedia.readthedocs.io",
+    ],
+)
+print("\nExcluding specified domains related to wikipedia:\n")
+for result in search_result:
+    print(result['url'])
