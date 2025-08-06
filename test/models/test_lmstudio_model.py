5bafc7c20e8304f2058038b417edd6542564205b
Wendong-Fan <w3ndong.fan@gmail.com>
2025-08-06 20:45:16 +0800

    update

--- test/models/test_lmstudio_model.py
+++ test/models/test_lmstudio_model.py
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========= Copyright 2023-2024 @ CAMEL-AI.org. All Rights Reserved. =========
-import re
 
 import pytest
 
@@ -36,23 +35,6 @@ def test_lmstudio_model(model_type: ModelType):
     assert isinstance(model.token_counter, OpenAITokenCounter)
     assert isinstance(model.model_type.value_for_tiktoken, str)
     assert isinstance(model.model_type.token_limit, int)
-
-
-@pytest.mark.model_backend
-def test_lmstudio_model_unexpected_argument():
-    model_type = ModelType.LMSTUDIO_GEMMA_3_1B
-    model_config_dict = {"model_path": "vicuna_70B"}
-
-    with pytest.raises(
-        ValueError,
-        match=re.escape(
-            (
-                "Unexpected argument `model_path` is "
-                "input into LMStudio model backend."
-            )
-        ),
-    ):
-        _ = LMStudioModel(model_type, model_config_dict)
 
 
 @pytest.mark.model_backend
