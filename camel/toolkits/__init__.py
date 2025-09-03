4730e089a3b9e85a09d55e2401089fbd6abed9ed
Ol1ver0413 <790221864@qq.com>
2025-09-03 11:49:57 -0400

    resend_mcp_0.1

--- camel/toolkits/__init__.py
+++ camel/toolkits/__init__.py
@@ -75,6 +75,7 @@ from .klavis_toolkit import KlavisToolkit
 from .aci_toolkit import ACIToolkit
 from .origene_mcp_toolkit import OrigeneToolkit
 from .playwright_mcp_toolkit import PlaywrightMCPToolkit
+from .resend_mcp_toolkit import ResendMCPToolkit
 from .wolfram_alpha_toolkit import WolframAlphaToolkit
 from .task_planning_toolkit import TaskPlanningToolkit
 from .hybrid_browser_toolkit import HybridBrowserToolkit
@@ -150,6 +151,7 @@ __all__ = [
     'KlavisToolkit',
     'ACIToolkit',
     'PlaywrightMCPToolkit',
+    'ResendMCPToolkit',
     'WolframAlphaToolkit',
     'BohriumToolkit',
     'OpenAIImageToolkit',  # Backward compatibility
