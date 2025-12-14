4da5487fdaa5cefcce4cbcafd12634b96c2a30fa
Wendong-Fan <133094783+Wendong-Fan@users.noreply.github.com>
2025-12-14 16:46:52 +0800

    feat: Wokforce support configurable fail handling (#3488)

--- camel/societies/workforce/__init__.py
+++ camel/societies/workforce/__init__.py
@@ -14,7 +14,7 @@
 
 from .role_playing_worker import RolePlayingWorker
 from .single_agent_worker import SingleAgentWorker
-from .utils import PipelineTaskBuilder
+from .utils import FailureHandlingConfig, PipelineTaskBuilder, RecoveryStrategy
 from .workflow_memory_manager import WorkflowSelectionMethod
 from .workforce import Workforce, WorkforceMode
 
@@ -25,4 +25,6 @@ __all__ = [
     "SingleAgentWorker",
     "RolePlayingWorker",
     "WorkflowSelectionMethod",
+    "FailureHandlingConfig",
+    "RecoveryStrategy",
 ]
